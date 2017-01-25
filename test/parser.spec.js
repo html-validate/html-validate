1758599678eae167ee46f0d334d49ad812002b9b
David Sveningsson <ext@sidvind.com>
2017-01-25 01:36:59 +0100

    tests for attribute parsing

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -13,10 +13,19 @@ describe('parser', function(){
 		before(function(){
 			htmllint = new HtmlLint();
 			htmllint.addListener('*', function(event){
-				events.push({
-					event: event.event,
-					tagName: event.target ? event.target.tagName : undefined,
-				});
+				if ( ['tag:open', 'tag:close'].indexOf(event.event) >= 0 ){
+					events.push({
+						event: event.event,
+						tagName: event.target ? event.target.tagName : undefined,
+					});
+				}
+				if ( ['attr'].indexOf(event.event) >= 0 ){
+					events.push({
+						event: event.event,
+						key: event.key,
+						value: event.value,
+					});
+				}
 			});
 		});
 
@@ -46,6 +55,42 @@ describe('parser', function(){
 
 		it('void elements', function(){
 			expect(htmllint.string('<input>')).to.be.true;
+		});
+
+		describe('attributes', function(){
+
+			it('with single quotes', function(){
+				expect(htmllint.string('<div foo=\'bar\'></div>')).to.be.true;
+				expect(events).to.have.lengthOf(3);
+				expect(events[0]).to.deep.equal({event: 'attr', key: 'foo', value: 'bar'});
+				expect(events[1]).to.deep.equal({event: 'tag:open', tagName: 'div'});
+				expect(events[2]).to.deep.equal({event: 'tag:close', tagName: 'div'});
+			});
+
+			it('with double quote', function(){
+				expect(htmllint.string('<div foo="bar"></div>')).to.be.true;
+				expect(events).to.have.lengthOf(3);
+				expect(events[0]).to.deep.equal({event: 'attr', key: 'foo', value: 'bar'});
+				expect(events[1]).to.deep.equal({event: 'tag:open', tagName: 'div'});
+				expect(events[2]).to.deep.equal({event: 'tag:close', tagName: 'div'});
+			});
+
+			it('without value', function(){
+				expect(htmllint.string('<div foo></div>')).to.be.true;
+				expect(events).to.have.lengthOf(3);
+				expect(events[0]).to.deep.equal({event: 'attr', key: 'foo', value: undefined});
+				expect(events[1]).to.deep.equal({event: 'tag:open', tagName: 'div'});
+				expect(events[2]).to.deep.equal({event: 'tag:close', tagName: 'div'});
+			});
+
+			it('with dashes', function(){
+				expect(htmllint.string('<div foo-bar-baz></div>')).to.be.true;
+				expect(events).to.have.lengthOf(3);
+				expect(events[0]).to.deep.equal({event: 'attr', key: 'foo-bar-baz', value: undefined});
+				expect(events[1]).to.deep.equal({event: 'tag:open', tagName: 'div'});
+				expect(events[2]).to.deep.equal({event: 'tag:close', tagName: 'div'});
+			});
+
 		});
 
 	});
