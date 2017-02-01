e0aa7d9c6389461b6fa03b7d68a203f4c9386a9e
David Sveningsson <ext@sidvind.com>
2017-02-01 22:12:20 +0100

    build: build via grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,20 @@
 module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-eslint');
 	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks("grunt-ts");
 
 	grunt.registerTask('test', ['eslint', 'mochaTest']);
-	grunt.registerTask('default', ['test']);
+	grunt.registerTask('build', ['ts', 'test']);
+	grunt.registerTask('default', ['build']);
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+
+		ts: {
+			build: {
+				tsconfig: './tsconfig.json',
+			},
+		},
 
 		eslint: {
 			build: [
