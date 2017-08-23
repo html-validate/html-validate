fae9b7d269944a1daedcf79402b88389ec63e474
David Sveningsson <ext@sidvind.com>
2017-08-23 19:10:49 +0200

    dom: refactor combinator

---
+++ src/dom/combinator.ts
@@ -0,0 +1,23 @@
+export enum Combinator {
+	DESCENDANT,
+	CHILD,
+	ADJACENT_SIBLING,
+	GENERAL_SIBLING,
+}
+
+export function parseCombinator(combinator: string): Combinator {
+	switch (combinator){
+	case undefined:
+	case null:
+	case '':
+		return Combinator.DESCENDANT;
+	case '>':
+		return Combinator.CHILD;
+	case '+':
+		return Combinator.ADJACENT_SIBLING;
+	case '~':
+		return Combinator.GENERAL_SIBLING;
+	default:
+		throw new Error(`Unknown combinator "${combinator}"`);
+	}
+}
