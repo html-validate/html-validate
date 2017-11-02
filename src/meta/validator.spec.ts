84b326c799c0b5485b87567194857e72d9909bf6
David Sveningsson <ext@sidvind.com>
2017-11-03 00:53:48 +0100

    elements: update html5 metadata with implicitClosed

--- src/meta/validator.spec.ts
+++ src/meta/validator.spec.ts
@@ -344,6 +344,7 @@ function mockEntry(tagName: string, stub = {}): MetaElement {
 		deprecated: false,
 		void: false,
 		transparent: false,
+		implicitClosed: [],
 		deprecatedAttributes: [],
 		permittedContent: [],
 		permittedDescendants: [],
