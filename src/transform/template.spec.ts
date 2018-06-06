4706a31170a0bcbb87aa46dc18158614530e4e62
David Sveningsson <ext@sidvind.com>
2018-06-06 22:50:09 +0200

    transform: more test cases

--- src/transform/template.spec.ts
+++ src/transform/template.spec.ts
@@ -4,19 +4,51 @@ describe('TemplateExtractor', function(){
 
 	const expect = require('chai').expect;
 
-	it('should extract templates from object property', () => {
+	describe('extractObjectProperty()', () => {
+
+		it('should extract templates from object property', () => {
+			const te = TemplateExtractor.fromString('foo({template: "<b>foo</b>"})');
+			expect(te.extractObjectProperty('template')).to.deep.equal([
+				'<b>foo</b>',
+			]);
+		});
+
+		it('should handle single quotes', () => {
+			const te = TemplateExtractor.fromString('foo({template: \'<b>foo</b>\'})');
+			expect(te.extractObjectProperty('template')).to.deep.equal([
+				'<b>foo</b>',
+			]);
+		});
+
+		it('should handle double quotes', () => {
+			const te = TemplateExtractor.fromString('foo({template: "<b>foo</b>"})');
+			expect(te.extractObjectProperty('template')).to.deep.equal([
+				'<b>foo</b>',
+			]);
+		});
+
+		it('should handle template literal', () => {
+			const te = TemplateExtractor.fromString('foo({template: `<b>foo</b>`})');
+			expect(te.extractObjectProperty('template')).to.deep.equal([
+				'<b>foo</b>',
+			]);
+		});
+
+		it('should handle tagged template', () => {
+			const te = TemplateExtractor.fromString('foo({template: foo`<b>foo</b>`})');
+			expect(te.extractObjectProperty('template')).to.deep.equal([
+				'<b>foo</b>',
+			]);
+		});
+
+	});
+
+	it('should extract from file', () => {
 		const te = TemplateExtractor.fromFilename('test-files/extract.js');
 		expect(te.extractObjectProperty('template')).to.deep.equal([
 			'<p>foo</i>',
 			'<b>foo</b>',
 			'<p>foo</p>',
-		]);
-	});
-
-	it('should extract from string source', () => {
-		const te = TemplateExtractor.fromString('const x = {template: "<b>foo</b>"}');
-		expect(te.extractObjectProperty('template')).to.deep.equal([
-			'<b>foo</b>',
 		]);
 	});
 
