bb3625e33e8006be534add43e0ed5f3750f75812
David Sveningsson <ext@sidvind.com>
2018-06-17 20:49:39 +0200

    reporter: add testcases for valid

--- src/reporter.spec.ts
+++ src/reporter.spec.ts
@@ -40,6 +40,44 @@ describe('Reporter', function(){
 
 	});
 
+	describe('save()', () => {
+
+		it('should set valid to true if there are no errors', () => {
+			const report = new Reporter();
+			expect(report.save().valid).to.be.true;
+		});
+
+		it('should set valid to true if there are only warnings', () => {
+			const report = new Reporter();
+			report.addManual("filename", createMessage("warning", 1));
+			expect(report.save().valid).to.be.true;
+
+		});
+
+		it('should set valid to false if there are any errors', () => {
+			const report = new Reporter();
+			report.addManual("filename", createMessage("error", 2));
+			expect(report.save().valid).to.be.false;
+		});
+
+		it('should set results', () => {
+			const report = new Reporter();
+			report.addManual("foo.html", createMessage("error", 2));
+			report.addManual("foo.html", createMessage("warning", 1));
+			report.addManual("bar.html", createMessage("another error", 2));
+			expect(report.save().results).to.deep.equal([
+				{filePath: 'foo.html', errorCount: 1, warningCount: 1, messages: [
+					{line: 1, column: 1, ruleId: 'mock', severity: 2, message: 'error'},
+					{line: 1, column: 1, ruleId: 'mock', severity: 1, message: 'warning'},
+				]},
+				{filePath: 'bar.html', errorCount: 1, warningCount: 0, messages: [
+					{line: 1, column: 1, ruleId: 'mock', severity: 2, message: 'another error'},
+				]},
+			]);
+		});
+
+	});
+
 });
 
 function createResult(filename: string, messages: string[]){
@@ -49,11 +87,11 @@ function createResult(filename: string, messages: string[]){
 	};
 }
 
-function createMessage(text: string): Message {
+function createMessage(message: string, severity: number = 2): Message {
 	return {
 		ruleId: 'mock',
-		severity: 2,
-		message: text,
+		severity,
+		message,
 		line: 1,
 		column: 1,
 	};
