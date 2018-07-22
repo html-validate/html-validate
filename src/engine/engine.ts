997318e565eb581cc11eaadcd0ad7e7b6ff6ed79
David Sveningsson <ext@sidvind.com>
2018-07-22 15:43:35 +0200

    engine: move other modes

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -1,9 +1,10 @@
 import { Config } from "../config";
 import { Location, Source } from "../context";
-import { InvalidTokenError } from "../lexer";
+import { InvalidTokenError, Lexer, TokenType } from "../lexer";
 import { Parser } from "../parser";
-import { Reporter } from "../reporter";
+import { Reporter, Report } from "../reporter";
 import { Rule } from "../rule";
+import { DOMNode } from "../dom";
 
 export class Engine {
 	report: Reporter;
@@ -14,10 +15,31 @@ export class Engine {
 		this.config = config;
 	}
 
+	public process(source: Source[], mode?: string): Report {
+		switch (mode){
+		case 'lint':
+		case undefined:
+			return this.parse(source);
+		case 'dump-events':
+			return this.dumpEvents(source);
+		case 'dump-tokens':
+			return this.dumpTokens(source);
+		case 'dump-tree':
+			return this.dumpTree(source);
+		default:
+			throw new Error(`Unknown mode "${mode}"`);
+		}
+	}
+
 	/**
-	 * Generate report for given sources.
+	 * Internal parse method.
+	 *
+	 * @param src {object} - Parse source.
+	 * @param src.data {string} - Text HTML data.
+	 * @param src.filename {string} - Filename of source for presentation in report.
+	 * @return {object} - Report output.
 	 */
-	public process(sources: Source[]){
+	private parse(sources: Source[]): Report{
 		const rules = this.config.getRules();
 
 		for (const source of sources){
@@ -46,10 +68,81 @@ export class Engine {
 		return this.report.save();
 	}
 
+	private dumpEvents(source: Source[]): Report {
+		const parser = new Parser(this.config);
+		const filtered = ['parent', 'children'];
+
+		parser.on('*', (event, data) => {
+			const strdata = JSON.stringify(data, (key, value) => {
+				return filtered.indexOf(key) >= 0 ? '[truncated]' : value;
+			}, 2);
+			process.stdout.write(`${event}: ${strdata}\n`);
+		});
+		source.forEach(src => parser.parseHtml(src));
+
+		return {
+			valid: true,
+			results: [],
+		};
+	}
+
+	private dumpTokens(source: Source[]): Report {
+		const lexer = new Lexer();
+		for (const src of source){
+			for (const token of lexer.tokenize(src)){
+				const data = token.data ? token.data[0] : null;
+				process.stdout.write(`TOKEN: ${TokenType[token.type]}
+  Data: ${JSON.stringify(data)}
+  Location: ${token.location.filename}:${token.location.line}:${token.location.column}
+`);
+			}
+		}
+		return {
+			valid: true,
+			results: [],
+		};
+	}
+
+	private dumpTree(source: Source[]): Report {
+		const parser = new Parser(this.config);
+		const dom = parser.parseHtml(source[0]); /* @todo handle dumping each tree */
+
+		function decoration(node: DOMNode){
+			let output = '';
+			if (node.hasAttribute('id')){
+				output += `#${node.getAttribute('id')}`;
+			}
+			if (node.hasAttribute('class')){
+				output += `.${node.classList.join('.')}`;
+			}
+			return output;
+		}
+
+		function printNode(node: DOMNode, level: number, sibling: number){
+			if (level > 0){
+				const indent = '  '.repeat(level - 1);
+				const l = node.children.length > 0 ? '┬' : '─';
+				const b = sibling < (node.parent.children.length - 1) ? '├' : '└';
+				process.stdout.write(`${indent}${b}─${l} ${node.tagName}${decoration(node)}\n`);
+			} else {
+				process.stdout.write(`(root)\n`);
+			}
+
+			node.children.forEach((child, index) => printNode(child, level + 1, index));
+		}
+
+		printNode(dom.root, 0, 0);
+
+		return {
+			valid: true,
+			results: [],
+		};
+	}
+
 	/**
 	 * Load a rule using current config.
 	 */
-	private static loadRule(name: string, data: any, parser: Parser, report: Reporter){
+	private static loadRule(name: string, data: any, parser: Parser, report: Reporter): void {
 		const [severity, options] = data;
 		if (severity >= Config.SEVERITY_WARN){
 			let rule: Rule;
--- src/htmlvalidate.ts
+++ src/htmlvalidate.ts
@@ -1,10 +1,8 @@
 import { Config, ConfigLoader } from './config';
 import { Engine } from './engine';
 import { Parser } from './parser';
-import { DOMNode } from 'dom';
 import { Report } from './reporter';
 import { Source } from './context';
-import { Lexer, TokenType } from './lexer';
 
 class HtmlValidate {
 	private globalConfig: Config;
@@ -28,7 +26,8 @@ class HtmlValidate {
 			column: 1,
 		}];
 		const config = this.getConfigFor('inline');
-		return this.process(source, config, 'lint');
+		const engine = new Engine(config);
+		return engine.process(source, 'lint');
 	}
 
 	/**
@@ -40,110 +39,14 @@ class HtmlValidate {
 	public validateFile(filename: string, mode?: string): Report {
 		const config = this.getConfigFor(filename);
 		const source = config.transform(filename);
-		return this.process(source, config, mode);
-	}
-
-	private process(source: Source[], config: Config, mode?: string){
-		switch (mode){
-		case 'lint':
-		case undefined:
-			return this.parse(source, config);
-		case 'dump-events':
-			return this.dumpEvents(source, config);
-		case 'dump-tokens':
-			return this.dumpTokens(source);
-		case 'dump-tree':
-			return this.dumpTree(source, config);
-		default:
-			throw new Error(`Unknown mode "${mode}"`);
-		}
+		const engine = new Engine(config);
+		return engine.process(source, mode);
 	}
 
 	/**
-	 * Internal parse method.
-	 *
-	 * @param src {object} - Parse source.
-	 * @param src.data {string} - Text HTML data.
-	 * @param src.filename {string} - Filename of source for presentation in report.
-	 * @return {object} - Report output.
+	 * Get parser for given filename.
 	 */
-	private parse(source: Source[], config: Config): Report {
-		const engine = new Engine(config);
-		return engine.process(source);
-	}
-
-	private dumpEvents(source: Source[], config: Config): Report {
-		const parser = new Parser(config);
-		const filtered = ['parent', 'children'];
-
-		parser.on('*', (event, data) => {
-			const strdata = JSON.stringify(data, (key, value) => {
-				return filtered.indexOf(key) >= 0 ? '[truncated]' : value;
-			}, 2);
-			process.stdout.write(`${event}: ${strdata}\n`);
-		});
-		source.forEach(src => parser.parseHtml(src));
-
-		return {
-			valid: true,
-			results: [],
-		};
-	}
-
-	private dumpTokens(source: Source[]): Report {
-		const lexer = new Lexer();
-		for (const src of source){
-			for (const token of lexer.tokenize(src)){
-				const data = token.data ? token.data[0] : null;
-				process.stdout.write(`TOKEN: ${TokenType[token.type]}
-  Data: ${JSON.stringify(data)}
-  Location: ${token.location.filename}:${token.location.line}:${token.location.column}
-`);
-			}
-		}
-		return {
-			valid: true,
-			results: [],
-		};
-	}
-
-	private dumpTree(source: Source[], config: Config): Report {
-		const parser = new Parser(config);
-		const dom = parser.parseHtml(source[0]); /* @todo handle dumping each tree */
-
-		function decoration(node: DOMNode){
-			let output = '';
-			if (node.hasAttribute('id')){
-				output += `#${node.getAttribute('id')}`;
-			}
-			if (node.hasAttribute('class')){
-				output += `.${node.classList.join('.')}`;
-			}
-			return output;
-		}
-
-		function printNode(node: DOMNode, level: number, sibling: number){
-			if (level > 0){
-				const indent = '  '.repeat(level - 1);
-				const l = node.children.length > 0 ? '┬' : '─';
-				const b = sibling < (node.parent.children.length - 1) ? '├' : '└';
-				process.stdout.write(`${indent}${b}─${l} ${node.tagName}${decoration(node)}\n`);
-			} else {
-				process.stdout.write(`(root)\n`);
-			}
-
-			node.children.forEach((child, index) => printNode(child, level + 1, index));
-		}
-
-		printNode(dom.root, 0, 0);
-
-		return {
-			valid: true,
-			results: [],
-		};
-	}
-
-	public getParserFor(source: Source){
+	public getParserFor(source: Source): Parser {
 		const config = this.getConfigFor(source.filename);
 		return new Parser(config);
 	}
@@ -151,7 +54,7 @@ class HtmlValidate {
 	/**
 	 * Get configuration for given filename.
 	 */
-	getConfigFor(filename: string): Config {
+	private getConfigFor(filename: string): Config {
 		const loader = new ConfigLoader();
 		const config = loader.fromTarget(filename);
 		return this.globalConfig.merge(config);
