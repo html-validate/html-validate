2074080415333743c826f658005c35a7dfd1000e
David Sveningsson <ext@sidvind.com>
2018-08-15 22:53:31 +0200

    pattern: migrate tests

--- src/pattern.spec.ts
+++ src/pattern.spec.ts
@@ -1,38 +1,35 @@
 import { parsePattern } from './pattern';
 
 describe('Pattern', function(){
-
-	const expect = require('chai').expect;
-
 	it('kebabcase should match strings with dashes', function(){
 		const pattern = parsePattern('kebabcase');
-		expect('foo-bar').to.match(pattern);
-		expect('fooBar').to.not.match(pattern);
-		expect('Foobar').to.not.match(pattern);
-		expect('foo_bar').to.not.match(pattern);
+		expect('foo-bar').toMatch(pattern);
+		expect('fooBar').not.toMatch(pattern);
+		expect('Foobar').not.toMatch(pattern);
+		expect('foo_bar').not.toMatch(pattern);
 	});
 
 	it('camelcase should match strings in camelcase', function(){
 		const pattern = parsePattern('camelcase');
-		expect('foo-bar').to.not.match(pattern);
-		expect('fooBar').to.match(pattern);
-		expect('Foobar').to.not.match(pattern);
-		expect('foo_bar').to.not.match(pattern);
+		expect('foo-bar').not.toMatch(pattern);
+		expect('fooBar').toMatch(pattern);
+		expect('Foobar').not.toMatch(pattern);
+		expect('foo_bar').not.toMatch(pattern);
 	});
 
 	it('underscore should match strings with underscore', function(){
 		const pattern = parsePattern('underscore');
-		expect('foo-bar').to.not.match(pattern);
-		expect('fooBar').to.not.match(pattern);
-		expect('Foobar').to.not.match(pattern);
-		expect('foo_bar').to.match(pattern);
+		expect('foo-bar').not.toMatch(pattern);
+		expect('fooBar').not.toMatch(pattern);
+		expect('Foobar').not.toMatch(pattern);
+		expect('foo_bar').toMatch(pattern);
 	});
 
 	it('should support user-supplied regexp', function(){
 		const pattern = parsePattern('^foo-[a-z]\\w+$');
-		expect('foo-bar').to.match(pattern);
-		expect('bar-foo').to.not.match(pattern);
-		expect('barfoo-baz').to.not.match(pattern);
+		expect('foo-bar').toMatch(pattern);
+		expect('bar-foo').not.toMatch(pattern);
+		expect('barfoo-baz').not.toMatch(pattern);
 	});
 
 });
