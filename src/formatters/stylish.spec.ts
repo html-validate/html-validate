b38148d8adc9a658502e37267a0c1f2ffae84b1a
David Sveningsson <ext@sidvind.com>
2018-10-17 23:12:40 +0200

    formatters: force color on during stylish test

--- src/formatters/stylish.spec.ts
+++ src/formatters/stylish.spec.ts
@@ -1,4 +1,12 @@
+/* force colors on when running stylish tests */
+const defaultColor = process.env.FORCE_COLOR;
+process.env.FORCE_COLOR = '1';
+
 const formatter = require('./stylish');
+
+/* restore color, need only to be set when importing library */
+process.env.FORCE_COLOR = defaultColor;
+
 import { Result } from '../reporter';
 
 describe('stylish formatter', () => {
