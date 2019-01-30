de052fbe9f35cbfd52ecdc93ae7c6678ca6b2b11
David Sveningsson <ext@sidvind.com>
2019-01-30 21:05:45 +0100

    elements: em

--- src/elements.spec.ts
+++ src/elements.spec.ts
@@ -236,6 +236,7 @@ describe("HTML elements", () => {
 		"div",
 		"dl",
 		"dt",
+		"em",
 	];
 
 	for (const tagName of tagNames) {
@@ -267,10 +268,6 @@ describe("HTML elements", () => {
 		disallowAttribute("input", "draggable", ["", "foobar"], "omit");
 		disallowAttribute("input", "hidden", ["foobar"], "omit");
 		disallowAttribute("input", "tabindex", ["", "foobar"], "omit");
-	});
-
-	describe("<em>", () => {
-		defaultTextLevel("em");
 	});
 
 	describe("<embed>", () => {
