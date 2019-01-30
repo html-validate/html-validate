da5878568badf5ed9b756119efdcb28b4f71d3fa
David Sveningsson <ext@sidvind.com>
2019-01-30 22:42:50 +0100

    elements: bgsound

--- src/elements.spec.ts
+++ src/elements.spec.ts
@@ -217,6 +217,7 @@ describe("HTML elements", () => {
 		"basefont",
 		"bdi",
 		"bdo",
+		"bgsound",
 	];
 
 	for (const tagName of tagNames) {
@@ -248,10 +249,6 @@ describe("HTML elements", () => {
 		disallowAttribute("input", "draggable", ["", "foobar"], "omit");
 		disallowAttribute("input", "hidden", ["foobar"], "omit");
 		disallowAttribute("input", "tabindex", ["", "foobar"], "omit");
-	});
-
-	describe("<bgsound>", () => {
-		deprecated("bgsound");
 	});
 
 	describe("<big>", () => {
