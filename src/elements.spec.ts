6aab13bebad156c34e0116d78a9aae4c13bdbf47
David Sveningsson <ext@sidvind.com>
2019-01-31 21:34:19 +0100

    elements: br

--- src/elements.spec.ts
+++ src/elements.spec.ts
@@ -222,6 +222,7 @@ describe("HTML elements", () => {
 		"blink",
 		"blockquote",
 		"body",
+		"br",
 	];
 
 	for (const tagName of tagNames) {
@@ -253,12 +254,6 @@ describe("HTML elements", () => {
 		disallowAttribute("input", "draggable", ["", "foobar"], "omit");
 		disallowAttribute("input", "hidden", ["foobar"], "omit");
 		disallowAttribute("input", "tabindex", ["", "foobar"], "omit");
-	});
-
-	describe("<br>", () => {
-		omitEnd("br");
-		allowParent("br", "span", "void");
-		allowParent("br", "div", "void");
 	});
 
 	describe("<button>", () => {
