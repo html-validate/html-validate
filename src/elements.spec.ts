eb35d385bf55e781071a47e07c3f31e52e1b95d8
David Sveningsson <ext@sidvind.com>
2019-02-05 23:04:46 +0100

    elements: global attributes

--- src/elements.spec.ts
+++ src/elements.spec.ts
@@ -1,27 +1,143 @@
 import HtmlValidate from "./htmlvalidate";
 import "./matchers";
 
-function getElementMarkup(
-	tagName: string,
-	variant: string,
-	attr: { [key: string]: string } = {}
-) {
-	const attrString = Object.entries(attr).reduce((str, [key, value]) => {
-		if (value !== undefined) {
-			return `${str} ${key}="${value}"`;
-		} else {
-			return `${str} ${key}`;
-		}
-	}, "");
-	switch (variant) {
-		case "omit":
-			return `<${tagName}${attrString}>`;
-		case "void":
-			return `<${tagName}${attrString}/>`;
-		default:
-			return `<${tagName}${attrString}>foo</${tagName}>`;
-	}
-}
+const fileDirectory = "test-files/elements";
+const tagNames = [
+	"a",
+	"abbr",
+	"acronym",
+	"address",
+	"applet",
+	"area",
+	"article",
+	"aside",
+	"audio",
+	"b",
+	"base",
+	"basefont",
+	"bdi",
+	"bdo",
+	"bgsound",
+	"big",
+	"blink",
+	"blockquote",
+	"body",
+	"br",
+	"button",
+	"canvas",
+	"caption",
+	"center",
+	"cite",
+	"code",
+	"col",
+	"colgroup",
+	"data",
+	"datalist",
+	"dd",
+	"del",
+	"dfn",
+	"dir",
+	"div",
+	"dl",
+	"dt",
+	"em",
+	"embed",
+	"fieldset",
+	"figcaption",
+	"figure",
+	"font",
+	"footer",
+	"form",
+	"frame",
+	"frameset",
+	"h1",
+	"h2",
+	"h3",
+	"h4",
+	"h5",
+	"h6",
+	"head",
+	"header",
+	"hgroup",
+	"hr",
+	"html",
+	"i",
+	"iframe",
+	"img",
+	"input",
+	"ins",
+	"isindex",
+	"kbd",
+	"keygen",
+	"label",
+	"legend",
+	"li",
+	"link",
+	"listing",
+	"main",
+	"map",
+	"mark",
+	"marquee",
+	"math",
+	"meta",
+	"meter",
+	"multicol",
+	"nav",
+	"nextid",
+	"nobr",
+	"noembed",
+	"noframes",
+	"noscript",
+	"object",
+	"ol",
+	"optgroup",
+	"option",
+	"output",
+	"p",
+	"param",
+	"plaintext",
+	"pre",
+	"progress",
+	"q",
+	"rb",
+	"rp",
+	"rt",
+	"rtc",
+	"ruby",
+	"s",
+	"samp",
+	"script",
+	"section",
+	"select",
+	"small",
+	"source",
+	"spacer",
+	"span",
+	"strike",
+	"strong",
+	"style",
+	"sub",
+	"sup",
+	"svg",
+	"table",
+	"tbody",
+	"td",
+	"textarea",
+	"tfoot",
+	"th",
+	"thead",
+	"time",
+	"title",
+	"tr",
+	"track",
+	"tt",
+	"u",
+	"ul",
+	"var",
+	"video",
+	"wbr",
+	"xmp",
+];
 
 describe("HTML elements", () => {
 	const htmlvalidate = new HtmlValidate({
@@ -35,185 +151,23 @@ describe("HTML elements", () => {
 		},
 	});
 
-	function allowAttribute(
-		tagName: string,
-		attr: string,
-		values: string[],
-		variant?: string
-	) {
-		if (values.length === 0) {
-			it(`should allow boolean attribute ${attr}`, () => {
-				const markup = getElementMarkup(tagName, variant, {
-					[attr]: undefined,
-				});
-				const report = htmlvalidate.validateString(markup);
-				expect(report).toBeValid();
-			});
-		}
-		for (const value of values) {
-			it(`should allow attribute ${attr}="${value}"`, () => {
-				const markup = getElementMarkup(tagName, variant, { [attr]: value });
-				const report = htmlvalidate.validateString(markup);
-				expect(report).toBeValid();
-			});
-		}
-	}
+	describe(`global attributes`, () => {
+		it("valid markup", () => {
+			const filename = `${fileDirectory}/global-attributes-valid.html`;
+			const report = htmlvalidate.validateFile(filename);
+			expect(report.results).toMatchSnapshot();
+		});
 
-	function disallowAttribute(
-		tagName: string,
-		attr: string,
-		values: string[],
-		variant?: string
-	) {
-		for (const value of values) {
-			it(`should disallow attribute ${attr}="${value}"`, () => {
-				const markup = getElementMarkup(tagName, variant, { [attr]: value });
-				const report = htmlvalidate.validateString(markup);
-				expect(report).toBeInvalid();
-			});
-		}
-	}
-
-	const tagNames = [
-		"a",
-		"abbr",
-		"acronym",
-		"address",
-		"applet",
-		"area",
-		"article",
-		"aside",
-		"audio",
-		"b",
-		"base",
-		"basefont",
-		"bdi",
-		"bdo",
-		"bgsound",
-		"big",
-		"blink",
-		"blockquote",
-		"body",
-		"br",
-		"button",
-		"canvas",
-		"caption",
-		"center",
-		"cite",
-		"code",
-		"col",
-		"colgroup",
-		"data",
-		"datalist",
-		"dd",
-		"del",
-		"dfn",
-		"dir",
-		"div",
-		"dl",
-		"dt",
-		"em",
-		"embed",
-		"fieldset",
-		"figcaption",
-		"figure",
-		"font",
-		"footer",
-		"form",
-		"frame",
-		"frameset",
-		"h1",
-		"h2",
-		"h3",
-		"h4",
-		"h5",
-		"h6",
-		"head",
-		"header",
-		"hgroup",
-		"hr",
-		"html",
-		"i",
-		"iframe",
-		"img",
-		"input",
-		"ins",
-		"isindex",
-		"kbd",
-		"keygen",
-		"label",
-		"legend",
-		"li",
-		"link",
-		"listing",
-		"main",
-		"map",
-		"mark",
-		"marquee",
-		"math",
-		"meta",
-		"meter",
-		"multicol",
-		"nav",
-		"nextid",
-		"nobr",
-		"noembed",
-		"noframes",
-		"noscript",
-		"object",
-		"ol",
-		"optgroup",
-		"option",
-		"output",
-		"p",
-		"param",
-		"plaintext",
-		"pre",
-		"progress",
-		"q",
-		"rb",
-		"rp",
-		"rt",
-		"rtc",
-		"ruby",
-		"s",
-		"samp",
-		"script",
-		"section",
-		"select",
-		"small",
-		"source",
-		"spacer",
-		"span",
-		"strike",
-		"strong",
-		"style",
-		"sub",
-		"sup",
-		"svg",
-		"table",
-		"tbody",
-		"td",
-		"textarea",
-		"tfoot",
-		"th",
-		"thead",
-		"time",
-		"title",
-		"tr",
-		"track",
-		"tt",
-		"u",
-		"ul",
-		"var",
-		"video",
-		"wbr",
-		"xmp",
-	];
+		it("invalid markup", () => {
+			const filename = `${fileDirectory}/global-attributes-invalid.html`;
+			const report = htmlvalidate.validateFile(filename);
+			expect(report.results).toMatchSnapshot();
+		});
+	});
 
 	for (const tagName of tagNames) {
-		const dir = "test-files/elements";
-		const filename = (variant: string) => `${dir}/${tagName}-${variant}.html`;
+		const filename = (variant: string) =>
+			`${fileDirectory}/${tagName}-${variant}.html`;
 
 		describe(`<${tagName}>`, () => {
 			it("valid markup", () => {
@@ -227,18 +181,4 @@ describe("HTML elements", () => {
 			});
 		});
 	}
-
-	describe("global attributes", () => {
-		allowAttribute("input", "contenteditable", ["", "true", "false"], "omit");
-		allowAttribute("input", "dir", ["ltr", "rtl", "auto"], "omit");
-		allowAttribute("input", "draggable", ["true", "false"], "omit");
-		allowAttribute("input", "hidden", [], "omit");
-		allowAttribute("input", "hidden", ["", "hidden"], "omit");
-		allowAttribute("input", "tabindex", ["0", "12", "-1"], "omit");
-		disallowAttribute("input", "contenteditable", ["foobar"], "omit");
-		disallowAttribute("input", "dir", ["", "foobar"], "omit");
-		disallowAttribute("input", "draggable", ["", "foobar"], "omit");
-		disallowAttribute("input", "hidden", ["foobar"], "omit");
-		disallowAttribute("input", "tabindex", ["", "foobar"], "omit");
-	});
 });
