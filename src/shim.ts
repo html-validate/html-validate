f5d76b732f2ed198022e5dd6e9198eb4aafd753d
David Sveningsson <ext@sidvind.com>
2019-02-23 13:07:52 +0100

    build: write shim in typescript

---
+++ src/shim.ts
@@ -0,0 +1,6 @@
+/* used when calling require('htmlvalidate'); */
+
+export { default as HtmlValidate } from "./htmlvalidate";
+export { DynamicValue } from "./dom/dynamic-value";
+export { Rule } from "./rule";
+export { TemplateExtractor } from "./transform/template";
