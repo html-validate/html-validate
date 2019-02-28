740edcf41b38d6e210e5b10eab2549b61981a0b7
David Sveningsson <ext@sidvind.com>
2019-02-28 08:46:31 +0100

    shim: sort lines

--- src/shim.ts
+++ src/shim.ts
@@ -1,9 +1,9 @@
 /* used when calling require('htmlvalidate'); */
 
 export { default as HtmlValidate } from "./htmlvalidate";
-export { DynamicValue } from "./dom/dynamic-value";
-export { Rule } from "./rule";
-export { TemplateExtractor } from "./transform/template";
-export { Source } from "./context";
 export { AttributeData } from "./parser";
 export { Config, ConfigLoader } from "./config";
+export { DynamicValue } from "./dom/dynamic-value";
+export { Rule } from "./rule";
+export { Source } from "./context";
+export { TemplateExtractor } from "./transform/template";
