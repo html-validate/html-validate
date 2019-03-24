fdabd08efd8b4b41ed5218f94f6de75eb8b8dc28
David Sveningsson <ext@sidvind.com>
2019-03-24 18:55:59 +0100

    rules: refactor text classification

---
+++ src/rules/helper/text.spec.ts
@@ -0,0 +1,42 @@
+import { Config } from "../../config";
+import { DynamicValue } from "../../dom";
+import { Parser } from "../../parser";
+import { classifyNodeText, TextClassification } from "./text";
+
+const parser = new Parser(Config.empty());
+
+describe("classifyNodeText()", () => {
+	it("should classify element with text as STATIC_TEXT", () => {
+		const node = parser.parseHtml("<p>lorem ipsum</p>").querySelector("p");
+		expect(classifyNodeText(node)).toEqual(TextClassification.STATIC_TEXT);
+	});
+
+	it("should classify element with descendant text as STATIC_TEXT", () => {
+		const node = parser
+			.parseHtml("<p><b>lorem ipsum</b></p>")
+			.querySelector("p");
+		expect(classifyNodeText(node)).toEqual(TextClassification.STATIC_TEXT);
+	});
+
+	it("should classify element without text as EMPTY_TEXT", () => {
+		const node = parser.parseHtml("<p></p>").querySelector("p");
+		expect(classifyNodeText(node)).toEqual(TextClassification.EMPTY_TEXT);
+	});
+
+	it("should classify element with only whitespace as EMPTY_TEXT", () => {
+		const node = parser.parseHtml("<p> </p>").querySelector("p");
+		expect(classifyNodeText(node)).toEqual(TextClassification.EMPTY_TEXT);
+	});
+
+	it("should classify element with dynamic text as DYNAMIC_TEXT", () => {
+		const node = parser.parseHtml("<p></p>").querySelector("p");
+		node.appendText(new DynamicValue(""));
+		expect(classifyNodeText(node)).toEqual(TextClassification.DYNAMIC_TEXT);
+	});
+
+	it("should classify element with descendant dynamic text as DYNAMIC_TEXT", () => {
+		const node = parser.parseHtml("<p>foo <b></b> bar</p>").querySelector("p");
+		node.querySelector("b").appendText(new DynamicValue(""));
+		expect(classifyNodeText(node)).toEqual(TextClassification.DYNAMIC_TEXT);
+	});
+});
---
+++ src/rules/helper/text.ts
@@ -0,0 +1,50 @@
+import { HtmlElement, NodeType, TextNode } from "../../dom";
+
+export enum TextClassification {
+	EMPTY_TEXT,
+	DYNAMIC_TEXT,
+	STATIC_TEXT,
+}
+
+/**
+ * Checks text content of an element.
+ *
+ * Any text is considered including text from descendant elements. Whitespace is
+ * ignored.
+ *
+ * If any text is dynamic `TextClassification.DYNAMIC_TEXT` is returned.
+ */
+export function classifyNodeText(node: HtmlElement): TextClassification {
+	const text = findTextNodes(node);
+
+	/* if any text is dynamic classify as dynamic */
+	if (text.some(cur => cur.isDynamic)) {
+		return TextClassification.DYNAMIC_TEXT;
+	}
+
+	/* if any text has non-whitespace character classify as static */
+	if (text.some(cur => cur.textContent.match(/\S/) !== null)) {
+		return TextClassification.STATIC_TEXT;
+	}
+
+	/* default to empty */
+	return TextClassification.EMPTY_TEXT;
+}
+
+function findTextNodes(node: HtmlElement): TextNode[] {
+	let text: TextNode[] = [];
+	for (const child of node.childNodes) {
+		switch (child.nodeType) {
+			case NodeType.TEXT_NODE:
+				text.push(child as TextNode);
+				break;
+			case NodeType.ELEMENT_NODE:
+				text = text.concat(findTextNodes(child as HtmlElement));
+				break;
+			/* istanbul ignore next: provides a sane default, nothing to test */
+			default:
+				break;
+		}
+	}
+	return text;
+}
