f152a12826c30846a7917fa24959748622037c69
David Sveningsson <ext@sidvind.com>
2019-09-15 22:27:47 +0200

    feat(cli): handle passing directories

--- src/cli/expand-files.ts
+++ src/cli/expand-files.ts
@@ -1,7 +1,14 @@
+import fs from "fs";
 import glob from "glob";
+import path from "path";
 
 interface ExpandOptions {
 	cwd?: string;
+}
+
+function isDirectory(filename: string): boolean {
+	const st = fs.statSync(filename);
+	return st.isDirectory();
 }
 
 /**
@@ -14,12 +21,28 @@ export function expandFiles(
 ): string[] {
 	const cwd = options.cwd || process.cwd();
 
-	const files = patterns.reduce((files: string[], pattern: string) => {
+	const files = patterns.reduce((result: string[], pattern: string) => {
 		/* process - as standard input */
 		if (pattern === "-") {
 			pattern = "/dev/stdin";
 		}
-		return files.concat(glob.sync(pattern, { cwd }));
+
+		for (const filename of glob.sync(pattern, { cwd })) {
+			/* if file is a directory recursively expand files from it */
+			const fullpath = path.join(cwd, filename);
+			if (isDirectory(fullpath)) {
+				const dir = expandFiles(
+					["**"],
+					Object.assign({}, options, { cwd: fullpath })
+				);
+				result = result.concat(dir.map(cur => path.join(filename, cur)));
+				continue;
+			}
+
+			result.push(filename);
+		}
+
+		return result;
 	}, []);
 
 	/* only return unique matches */
