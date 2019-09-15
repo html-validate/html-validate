420dc88ab861b4dda9b5109ba75c075b2fec44ff
David Sveningsson <ext@sidvind.com>
2019-09-15 22:05:34 +0200

    feat(cli): support setting cwd when calling expandFiles

--- src/cli/expand-files.ts
+++ src/cli/expand-files.ts
@@ -1,16 +1,25 @@
 import glob from "glob";
 
+interface ExpandOptions {
+	cwd?: string;
+}
+
 /**
  * Takes a number of file patterns (globs) and returns array of expanded
  * filenames.
  */
-export function expandFiles(patterns: string[]): string[] {
+export function expandFiles(
+	patterns: string[],
+	options: ExpandOptions = {}
+): string[] {
+	const cwd = options.cwd || process.cwd();
+
 	const files = patterns.reduce((files: string[], pattern: string) => {
 		/* process - as standard input */
 		if (pattern === "-") {
 			pattern = "/dev/stdin";
 		}
-		return files.concat(glob.sync(pattern));
+		return files.concat(glob.sync(pattern, { cwd }));
 	}, []);
 
 	/* only return unique matches */
