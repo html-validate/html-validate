f3445274d4e713e2c851bd524ebb429da9408abb
David Sveningsson <ext@sidvind.com>
2020-02-09 21:20:17 +0100

    feat: expose `NodeClosed`, `TextNode`, `Plugin` and `Parser`

--- src/shim.ts
+++ src/shim.ts
@@ -4,11 +4,13 @@ export { default as HtmlValidate } from "./htmlvalidate";
 export { AttributeData } from "./parser";
 export { CLI } from "./cli/cli";
 export { Config, ConfigData, ConfigLoader, Severity } from "./config";
-export { DynamicValue, HtmlElement } from "./dom";
+export { DynamicValue, HtmlElement, NodeClosed, TextNode } from "./dom";
 export { Rule, RuleDocumentation } from "./rule";
 export { Source, Location, ProcessElementContext } from "./context";
 export { Report, Reporter, Message, Result } from "./reporter";
 export { Transformer, TemplateExtractor } from "./transform";
+export { Plugin } from "./plugin";
+export { Parser } from "./parser";
 
 const pkg = require("../package.json");
 export const version = pkg.version;
