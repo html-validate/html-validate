1a896a87f8e7c253e4d21faf01aae6f52df754f8
David Sveningsson <ext@sidvind.com>
2020-03-08 00:23:38 +0100

    fix(rules): improve documentation for `doctype-html`

--- src/rules/doctype-html.ts
+++ src/rules/doctype-html.ts
@@ -4,8 +4,13 @@ import { Rule, RuleDocumentation, ruleDocumentationUrl } from "../rule";
 class NoStyleTag extends Rule {
 	public documentation(): RuleDocumentation {
 		return {
-			description:
-				'HTML5 documents should use the "html" doctype (short form, not legacy string)',
+			description: [
+				'HTML5 documents should use the "html" doctype (short `form`, not legacy string):',
+				"",
+				"```html",
+				"<!DOCTYPE html>",
+				"```",
+			].join("\n"),
 			url: ruleDocumentationUrl(__filename),
 		};
 	}
