2d002c79f1f6f088c1792de768ba80777629b739
David Sveningsson <ext@sidvind.com>
2020-03-08 17:23:33 +0100

    fix(shim): expose `ConfigError` and `UserError`

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export { Config } from "./config";
 export { ConfigData, RuleConfig } from "./config-data";
 export { ConfigLoader } from "./config-loader";
+export { ConfigError } from "./error";
 export { Severity } from "./severity";
--- src/shim.ts
+++ src/shim.ts
@@ -3,8 +3,15 @@
 export { default as HtmlValidate } from "./htmlvalidate";
 export { AttributeData } from "./parser";
 export { CLI } from "./cli/cli";
-export { Config, ConfigData, ConfigLoader, Severity } from "./config";
+export {
+	Config,
+	ConfigData,
+	ConfigError,
+	ConfigLoader,
+	Severity,
+} from "./config";
 export { DynamicValue, HtmlElement, NodeClosed, TextNode } from "./dom";
+export { UserError } from "./error";
 export { Rule, RuleDocumentation } from "./rule";
 export { Source, Location, ProcessElementContext } from "./context";
 export { Report, Reporter, Message, Result } from "./reporter";
