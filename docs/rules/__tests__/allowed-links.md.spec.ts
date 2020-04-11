d8762060c6a8b5b2f6a67cbbffd229b8232a7dfa
David Sveningsson <ext@sidvind.com>
2020-04-11 14:18:33 +0200

    feat(rules): new rule allowed-links

---
+++ docs/rules/__tests__/allowed-links.md.spec.ts
@@ -0,0 +1,62 @@
+import HtmlValidate from "../../../src/htmlvalidate";
+
+const markup: { [key: string]: string } = {};
+markup["external-invalid"] = `<a href="http://example.net/foo">`;
+markup["external-valid"] = `<a href="./foo">`;
+markup["relative-invalid"] = `<a href="../foo">`;
+markup["relative-valid"] = `<a href="/foo">`;
+markup["absolute-invalid"] = `<a href="/foo">`;
+markup["absolute-valid"] = `<a href="../foo">`;
+markup["base-invalid"] = `<a href="foo">`;
+markup["base-valid"] = `<a href="./foo">`;
+
+describe("docs/rules/allowed-links.md", () => {
+	it("inline validation: external-invalid", () => {
+		expect.assertions(1);
+		const htmlvalidate = new HtmlValidate({"rules":{"allowed-links":["error",{"allowExternal":false}]}});
+		const report = htmlvalidate.validateString(markup["external-invalid"]);
+		expect(report.results).toMatchSnapshot();
+	});
+	it("inline validation: external-valid", () => {
+		expect.assertions(1);
+		const htmlvalidate = new HtmlValidate({"rules":{"allowed-links":["error",{"allowExternal":false}]}});
+		const report = htmlvalidate.validateString(markup["external-valid"]);
+		expect(report.results).toMatchSnapshot();
+	});
+	it("inline validation: relative-invalid", () => {
+		expect.assertions(1);
+		const htmlvalidate = new HtmlValidate({"rules":{"allowed-links":["error",{"allowRelative":false}]}});
+		const report = htmlvalidate.validateString(markup["relative-invalid"]);
+		expect(report.results).toMatchSnapshot();
+	});
+	it("inline validation: relative-valid", () => {
+		expect.assertions(1);
+		const htmlvalidate = new HtmlValidate({"rules":{"allowed-links":["error",{"allowRelative":false}]}});
+		const report = htmlvalidate.validateString(markup["relative-valid"]);
+		expect(report.results).toMatchSnapshot();
+	});
+	it("inline validation: absolute-invalid", () => {
+		expect.assertions(1);
+		const htmlvalidate = new HtmlValidate({"rules":{"allowed-links":["error",{"allowAbsolute":false}]}});
+		const report = htmlvalidate.validateString(markup["absolute-invalid"]);
+		expect(report.results).toMatchSnapshot();
+	});
+	it("inline validation: absolute-valid", () => {
+		expect.assertions(1);
+		const htmlvalidate = new HtmlValidate({"rules":{"allowed-links":["error",{"allowAbsolute":false}]}});
+		const report = htmlvalidate.validateString(markup["absolute-valid"]);
+		expect(report.results).toMatchSnapshot();
+	});
+	it("inline validation: base-invalid", () => {
+		expect.assertions(1);
+		const htmlvalidate = new HtmlValidate({"rules":{"allowed-links":["error",{"allowBase":false}]}});
+		const report = htmlvalidate.validateString(markup["base-invalid"]);
+		expect(report.results).toMatchSnapshot();
+	});
+	it("inline validation: base-valid", () => {
+		expect.assertions(1);
+		const htmlvalidate = new HtmlValidate({"rules":{"allowed-links":["error",{"allowBase":false}]}});
+		const report = htmlvalidate.validateString(markup["base-valid"]);
+		expect(report.results).toMatchSnapshot();
+	});
+});
