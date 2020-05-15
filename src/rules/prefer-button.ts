b046dc5943a4bd05dff9766ea6b9c9f522c09d1a
David Sveningsson <ext@sidvind.com>
2020-05-15 22:29:03 +0200

    feat(rules): add `include` and `exclude` options to `prefer-button`
    refs #90

--- src/rules/prefer-button.ts
+++ src/rules/prefer-button.ts
@@ -1,37 +1,90 @@
-import { TagCloseEvent } from "../event";
+import { AttributeEvent } from "../event";
 import { Rule, RuleDocumentation, ruleDocumentationUrl } from "../rule";
+import { DynamicValue } from "../dom";
 
-export default class PreferButton extends Rule {
-	public documentation(): RuleDocumentation {
-		return {
+interface RuleContext {
+	type: string;
+}
+
+interface RuleOptions {
+	include: string[] | null;
+	exclude: string[] | null;
+}
+
+export const types = ["button", "submit", "reset", "image"];
+const replacement: Record<string, string> = {
+	button: '<button type="button">',
+	submit: '<button type="submit">',
+	reset: '<button type="reset">',
+	image: '<button type="button">',
+};
+
+const defaults: RuleOptions = {
+	include: null,
+	exclude: null,
+};
+
+export default class PreferButton extends Rule<RuleContext, RuleOptions> {
+	public constructor(options: RuleOptions) {
+		super({ ...defaults, ...options });
+	}
+
+	public documentation(context: RuleContext): RuleDocumentation {
+		const doc: RuleDocumentation = {
 			description: `Prefer to use the generic \`<button>\` element instead of \`<input>\`.`,
 			url: ruleDocumentationUrl(__filename),
 		};
+		if (context) {
+			const src = `<input type="${context.type}">`;
+			const dst = replacement[context.type] || `<button>`;
+			doc.description = `Prefer to use \`${dst}\` instead of \`"${src}\`.`;
+		}
+		return doc;
 	}
 
 	public setup(): void {
-		this.on("tag:close", (event: TagCloseEvent) => {
-			const node = event.previous;
+		this.on("attr", (event: AttributeEvent) => {
+			const node = event.target;
 
 			/* only handle input elements */
 			if (node.tagName !== "input") {
 				return;
 			}
 
-			const type = node.getAttribute("type");
-
-			/* sanity check: handle missing and boolean attributes */
-			if (!type || type.value === null) {
+			/* sanity check: handle missing, boolean and dynamic attributes */
+			if (!event.value || event.value instanceof DynamicValue) {
 				return;
 			}
 
-			if (type.valueMatches(/^(button|submit|reset|image)$/, false)) {
-				this.report(
-					node,
-					`Prefer to use <button> instead of <input type="${type.value}"> when adding buttons`,
-					type.valueLocation
-				);
+			/* ignore types configured to be ignored */
+			if (this.isIgnored(event.value)) {
+				return;
 			}
+
+			/* only values matching known type triggers error */
+			if (!types.includes(event.value)) {
+				return;
+			}
+
+			const context: RuleContext = { type: event.value };
+			const message = `Prefer to use <button> instead of <input type="${event.value}"> when adding buttons`;
+			this.report(node, message, event.valueLocation, context);
 		});
+	}
+
+	private isIgnored(type: string): boolean {
+		const { include, exclude } = this.options;
+
+		/* ignore roles not present in "include" */
+		if (include && !include.includes(type)) {
+			return true;
+		}
+
+		/* ignore roles present in "excludes" */
+		if (exclude && exclude.includes(type)) {
+			return true;
+		}
+
+		return false;
 	}
 }
