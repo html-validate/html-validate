d4445bb1453408afddf10113acf1db89afd30f7b
David Sveningsson <ext@sidvind.com>
2020-08-20 15:58:53 +0200

    feat(rules): new rule `no-redundant-for`
    fixes #87

---
+++ src/rules/no-redundant-for.ts
@@ -0,0 +1,32 @@
+import { ElementReadyEvent } from "../event";
+import { Rule, RuleDocumentation, ruleDocumentationUrl } from "../rule";
+
+export default class NoRedundantFor extends Rule {
+	public documentation(): RuleDocumentation {
+		return {
+			description: `When the \`<label>\` element wraps the labelable control the \`for\` attribute is redundant and better left out.`,
+			url: ruleDocumentationUrl(__filename),
+		};
+	}
+
+	public setup(): void {
+		this.on("element:ready", (event: ElementReadyEvent) => {
+			const { target } = event;
+
+			/* ignore label without for or dynamic value */
+			const attr = target.getAttribute("for");
+			if (!attr || attr.isDynamic) {
+				return;
+			}
+
+			/* try to find labeled control */
+			const id = attr.value;
+			const control = target.querySelector(`[id="${id}"]`);
+			if (!control) {
+				return;
+			}
+
+			this.report(target, 'Redundant "for" attribute', attr.keyLocation);
+		});
+	}
+}
