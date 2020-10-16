fa3c065f2968829bafd0c20ae52158d725be27ca
David Sveningsson <ext@sidvind.com>
2020-10-16 19:17:18 +0200

    feat(rules): new rule `no-multiple-main`

---
+++ docs/rules/no-multiple-main.md
@@ -0,0 +1,29 @@
+---
+docType: rule
+name: no-multiple-main
+category: content-model
+summary: Disallow multiple `<main>`
+---
+
+# Disallows multiple `<main>` elements in the same document (`no-multiple-main`)
+
+HTML5 [disallows][whatwg] multiple visible `<main>` element in the same document.
+Multiple `<main>` can be present but at most one can be visible and the others must be hidden using the `hidden` attribute.
+
+[whatwg]: https://html.spec.whatwg.org/multipage/grouping-content.html#the-main-element
+
+## Rule details
+
+Examples of **incorrect** code for this rule:
+
+<validate name="incorrect" rules="no-multiple-main">
+	<main>foo</main>
+	<main>bar</main>
+</validate>
+
+Examples of **correct** code for this rule:
+
+<validate name="correct" rules="no-multiple-main">
+	<main>foo</main>
+	<main hidden>bar</main>
+</validate>
---
+++ src/rules/no-multiple-main.ts
@@ -0,0 +1,28 @@
+import { DOMReadyEvent } from "../event";
+import { Rule, RuleDocumentation, ruleDocumentationUrl } from "../rule";
+
+export default class NoMultipleMain extends Rule {
+	public documentation(): RuleDocumentation {
+		return {
+			description: [
+				"Only a single visible `<main>` element can be present at in a document at a time.",
+				"",
+				"Multiple `<main>` can be present in the DOM as long the others are hidden using the HTML5 `hidden` attribute.",
+			].join("\n"),
+			url: ruleDocumentationUrl(__filename),
+		};
+	}
+
+	public setup(): void {
+		this.on("dom:ready", (event: DOMReadyEvent) => {
+			const { document } = event;
+			const main = document.querySelectorAll("main").filter((cur) => !cur.hasAttribute("hidden"));
+			main.shift(); /* ignore the first occurrence */
+
+			/* report all other occurrences */
+			for (const elem of main) {
+				this.report(elem, "Multiple <main> elements present in document");
+			}
+		});
+	}
+}
