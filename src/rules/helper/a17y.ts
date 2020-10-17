60c885a687d1a8f5d6609b58554bcf8b971aa61d
David Sveningsson <ext@sidvind.com>
2020-10-17 14:52:43 +0200

    refactor(rules): split `inAccessibilityTree` to separate functions
    Some rules should only consider `aria-hidden` but not `role`.

--- src/rules/helper/a17y.ts
+++ src/rules/helper/a17y.ts
@@ -2,11 +2,13 @@ import { HtmlElement } from "../../dom";
 
 declare module "../../dom/cache" {
 	export interface DOMNodeCache {
-		[CACHE_KEY]: boolean;
+		[ARIA_HIDDEN_CACHE]: boolean;
+		[ROLE_PRESENTATION_CACHE]: boolean;
 	}
 }
 
-const CACHE_KEY = Symbol(inAccessibilityTree.name);
+const ARIA_HIDDEN_CACHE = Symbol(isAriaHidden.name);
+const ROLE_PRESENTATION_CACHE = Symbol(isPresentation.name);
 
 /**
  * Tests if this element is present in the accessibility tree.
@@ -16,28 +18,58 @@ const CACHE_KEY = Symbol(inAccessibilityTree.name);
  * visible since the element might be in the visibility tree sometimes.
  */
 export function inAccessibilityTree(node: HtmlElement): boolean {
-	if (node.cacheExists(CACHE_KEY)) {
-		return node.cacheGet(CACHE_KEY);
+	return !isAriaHidden(node) && !isPresentation(node);
+}
+
+/**
+ * Tests if this element or an ancestor have `aria-hidden="true"`.
+ *
+ * Dynamic values yields `false` since the element will conditionally be in the
+ * accessibility tree and must fulfill it's conditions.
+ */
+export function isAriaHidden(node: HtmlElement): boolean {
+	if (node.cacheExists(ARIA_HIDDEN_CACHE)) {
+		return node.cacheGet(ARIA_HIDDEN_CACHE);
 	}
 
 	let cur: HtmlElement = node;
 	do {
-		const role = cur.getAttribute("role");
 		const ariaHidden = cur.getAttribute("aria-hidden");
-
-		/* role="presentation" */
-		if (role && role.value === "presentation") {
-			return cur.cacheSet(CACHE_KEY, false);
-		}
 
 		/* aria-hidden="true" */
 		if (ariaHidden && ariaHidden.value === "true") {
-			return cur.cacheSet(CACHE_KEY, false);
+			return cur.cacheSet(ARIA_HIDDEN_CACHE, true);
 		}
 
 		/* check parents */
 		cur = cur.parent;
 	} while (!cur.isRootElement());
 
-	return node.cacheSet(CACHE_KEY, true);
+	return node.cacheSet(ARIA_HIDDEN_CACHE, false);
+}
+
+/**
+ * Tests if this element or a parent element has role="presentation".
+ *
+ * Dynamic values yields `false` just as if the attribute wasn't present.
+ */
+export function isPresentation(node: HtmlElement): boolean {
+	if (node.cacheExists(ROLE_PRESENTATION_CACHE)) {
+		return node.cacheGet(ROLE_PRESENTATION_CACHE);
+	}
+
+	let cur: HtmlElement = node;
+	do {
+		const role = cur.getAttribute("role");
+
+		/* role="presentation" */
+		if (role && role.value === "presentation") {
+			return cur.cacheSet(ROLE_PRESENTATION_CACHE, true);
+		}
+
+		/* check parents */
+		cur = cur.parent;
+	} while (!cur.isRootElement());
+
+	return node.cacheSet(ROLE_PRESENTATION_CACHE, false);
 }
