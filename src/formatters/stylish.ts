2d1bc819bd241294db55fc28dd7305ee46d9ad3f
David Sveningsson <ext@sidvind.com>
2020-11-15 17:42:46 +0100

    fix(deps): replace dependency on `eslint` with `@html-validate/stylish`

--- src/formatters/stylish.ts
+++ src/formatters/stylish.ts
@@ -1,6 +1,16 @@
+import stylishImpl from "@html-validate/stylish";
+import { Result } from "../reporter";
 import { Formatter } from "./formatter";
 
-const stylish = require("eslint/lib/cli-engine/formatters/stylish");
+function stylish(results: Result[]): string {
+	return stylishImpl(
+		results.map((it) => ({
+			...it,
+			fixableErrorCount: 0,
+			fixableWarningCount: 0,
+		}))
+	);
+}
 
 const formatter: Formatter = stylish;
 export default formatter;
