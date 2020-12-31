a60ea99946047f54e3979925c21d9454fa52b04e
David Sveningsson <ext@sidvind.com>
2020-12-31 13:30:29 +0100

    refactor(error): remove casting

--- src/error/schema-validation-error.ts
+++ src/error/schema-validation-error.ts
@@ -1,26 +1,26 @@
 import betterAjvErrors from "@sidvind/better-ajv-errors";
-import { ErrorObject } from "ajv";
+import { ErrorObject, SchemaObject } from "ajv";
 import { UserError } from "./user-error";
 
 function getSummary(schema: any, obj: any, errors: ErrorObject[]): string {
 	const output = betterAjvErrors(schema, obj, errors, {
 		format: "js",
-	}) as any;
+	});
 	// istanbul ignore next: for safety only
 	return output.length > 0 ? output[0].error : "unknown validation error";
 }
 
 export class SchemaValidationError extends UserError {
 	public filename: string | null;
-	private obj: any;
-	private schema: any;
+	private obj: unknown;
+	private schema: SchemaObject;
 	private errors: ErrorObject[];
 
 	public constructor(
 		filename: string | null,
 		message: string,
-		obj: any, // eslint-disable-line @typescript-eslint/explicit-module-boundary-types
-		schema: any, // eslint-disable-line @typescript-eslint/explicit-module-boundary-types
+		obj: unknown,
+		schema: SchemaObject,
 		errors: ErrorObject[]
 	) {
 		const summary = getSummary(schema, obj, errors);
