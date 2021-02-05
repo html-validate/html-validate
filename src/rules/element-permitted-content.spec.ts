bef8a1663b70539091c203d5a4167446513904b9
David Sveningsson <ext@sidvind.com>
2021-02-05 06:10:47 +0100

    feat(meta): `transparent` can be limited to specific elements

--- src/rules/element-permitted-content.spec.ts
+++ src/rules/element-permitted-content.spec.ts
@@ -95,20 +95,84 @@ describe("rule element-permitted-content", () => {
 		);
 	});
 
-	it("should not report error when phrasing a-element is child of @phrasing", () => {
-		expect.assertions(1);
-		const report = htmlvalidate.validateString("<span><a><span></span></a></span>");
-		expect(report).toBeValid();
-	});
+	describe("transparent", () => {
+		it("should not report error when phrasing a-element is child of @phrasing", () => {
+			expect.assertions(1);
+			const report = htmlvalidate.validateString("<span><a><span></span></a></span>");
+			expect(report).toBeValid();
+		});
 
-	it("should report error when non-phrasing a-element is child of @phrasing", () => {
-		expect.assertions(2);
-		const report = htmlvalidate.validateString("<span><a><div></div></a></span>");
-		expect(report).toBeInvalid();
-		expect(report).toHaveError(
-			"element-permitted-content",
-			"Element <div> is not permitted as content in <span>"
-		);
+		it("should report error when non-phrasing a-element is child of @phrasing", () => {
+			expect.assertions(2);
+			const report = htmlvalidate.validateString("<span><a><div></div></a></span>");
+			expect(report).toBeInvalid();
+			expect(report).toHaveError(
+				"element-permitted-content",
+				"Element <div> is not permitted as content in <span>"
+			);
+		});
+
+		it("should report error for children listed as transparent", () => {
+			expect.assertions(2);
+			const htmlvalidate = new HtmlValidate({
+				elements: [
+					"html5",
+					{
+						"transparent-element": {
+							phrasing: true,
+							transparent: ["div"],
+						},
+					},
+				],
+				rules: { "element-permitted-content": "error" },
+			});
+			const markup = "<span><transparent-element><div></div></transparent-element></span>";
+			const report = htmlvalidate.validateString(markup);
+			expect(report).toBeInvalid();
+			expect(report).toHaveError(
+				"element-permitted-content",
+				"Element <div> is not permitted as content in <span>"
+			);
+		});
+
+		it("should not report error for children not listed as transparent", () => {
+			expect.assertions(1);
+			const htmlvalidate = new HtmlValidate({
+				elements: [
+					"html5",
+					{
+						"transparent-element": {
+							phrasing: true,
+							transparent: ["p"],
+						},
+					},
+				],
+				rules: { "element-permitted-content": "error" },
+			});
+			const markup = "<span><transparent-element><div></div></transparent-element></span>";
+			const report = htmlvalidate.validateString(markup);
+			expect(report).toBeValid();
+		});
+
+		it("should not report error for transparent unknown element children", () => {
+			expect.assertions(1);
+			const htmlvalidate = new HtmlValidate({
+				elements: [
+					"html5",
+					{
+						"transparent-element": {
+							phrasing: true,
+							transparent: ["@flow"],
+						},
+					},
+				],
+				rules: { "element-permitted-content": "error" },
+			});
+			const markup =
+				"<span><transparent-element><unknown-element></unknown-element></transparent-element></span>";
+			const report = htmlvalidate.validateString(markup);
+			expect(report).toBeValid();
+		});
 	});
 
 	it("should report error when label contains non-phrasing", () => {
