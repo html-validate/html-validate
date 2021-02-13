7a2d264f6ff805740a2c1ed3988327cd96441682
David Sveningsson <ext@sidvind.com>
2021-02-13 20:33:02 +0100

    feat(rules): new rule `no-utf8-bom` disallowing usage of UTF-8 BOM

---
+++ docs/rules/no-utf8-bom.md
@@ -0,0 +1,13 @@
+---
+docType: rule
+name: no-utf8-bom
+category: document
+summary: Disallow documents from having UTF-8 BOM
+---
+
+# Disallow documents from having UTF-8 BOM (`no-utf8-bom`)
+
+A unicode byte order mark (BOM) is needed in UTF-16 and UTF-32 to determine endiannes but for UTF-8 it has no meaning.
+Browsers are required to handle the BOM under all circumstances but tooling might not handle it properly and is therefor best left out.
+
+Instead the document should be served with the `Content-Type: application/javascript; charset=utf-8` header and/or the `<meta charset="utf-8">` meta-tag.
---
+++ src/rules/no-utf8-bom.ts
@@ -0,0 +1,24 @@
+import { TokenEvent } from "../event";
+import { TokenType } from "../lexer";
+import { Rule, RuleDocumentation, ruleDocumentationUrl } from "../rule";
+
+export default class NoUtf8Bom extends Rule {
+	public documentation(): RuleDocumentation {
+		return {
+			description: `This file is saved with the UTF-8 byte order mark (BOM) present. It is neither required or recommended to use.\n\nInstead the document should be served with the \`Content-Type: application/javascript; charset=utf-8\` header.`,
+			url: ruleDocumentationUrl(__filename),
+		};
+	}
+
+	public setup(): void {
+		const unregister = this.on("token", (event: TokenEvent) => {
+			if (event.type === TokenType.UNICODE_BOM) {
+				this.report(null, "File should be saved without UTF-8 BOM", event.location);
+			}
+
+			/* since the BOM must be the very first thing the rule can now be disabled for the rest of the run */
+			this.setEnabled(false);
+			unregister();
+		});
+	}
+}
