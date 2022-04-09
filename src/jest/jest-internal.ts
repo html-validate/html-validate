15f7e1c227ebc02ed0ca1728339dd27ee64d8735
David Sveningsson <ext@sidvind.com>
2022-04-09 09:44:30 +0200

    feat(jest): remove internal `toBeToken` matcher from public consumption

---
+++ src/jest/jest-internal.ts
@@ -0,0 +1,25 @@
+import "./jest";
+
+import { Location } from "../context";
+import { TokenType } from "../lexer";
+import { toBeToken } from "./matchers";
+
+interface TokenMatcher {
+	type: TokenType;
+	location?: Partial<Location>;
+	data?: any;
+}
+
+declare global {
+	/* eslint-disable-next-line @typescript-eslint/no-namespace */
+	namespace jest {
+		/* eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-unused-vars */
+		interface Matchers<R, T = {}> {
+			toBeToken(expected: TokenMatcher): R;
+		}
+	}
+}
+
+expect.extend({
+	toBeToken,
+});
