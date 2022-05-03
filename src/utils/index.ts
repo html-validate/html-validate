6be5c3a40e68f6a92fad9927973b2dffdb28ec20
David Sveningsson <ext@sidvind.com>
2022-05-03 18:37:37 +0200

    perf: improve performance by caching json schema

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export { compatibilityCheck, CompatibilityOptions } from "./compatibility-check";
+export { computeHash } from "./compute-hash";
 export { interpolate } from "./interpolate";
 export { requireUncached } from "./require-uncached";
 export { ruleExists } from "./rule-exists";
