cedf9d523ab100fb6312559da3defd8114cdff3d
David Sveningsson <ext@sidvind.com>
2022-05-14 18:07:39 +0200

    feat(rules): add `any` option to `attr-quotes`
    fixes #152

--- src/rules/attr-quotes.ts
+++ src/rules/attr-quotes.ts
@@ -7,10 +7,25 @@ enum QuoteStyle {
 	SINGLE_QUOTE = "'",
 	DOUBLE_QUOTE = '"',
 	AUTO_QUOTE = "auto",
+	ANY_QUOTE = "any",
 }
 
+export interface RuleStyleContext {
+	error: "style";
+	attr: string;
+	expected: string;
+	actual: string;
+}
+
+export interface RuleUnquotedContext {
+	error: "unquoted";
+	attr: string;
+}
+
+type RuleContext = RuleStyleContext | RuleUnquotedContext;
+
 interface RuleOptions {
-	style: "auto" | "single" | "double";
+	style: "auto" | "single" | "double" | "any";
 	unquoted: boolean;
 }
 
@@ -19,13 +34,51 @@ const defaults: RuleOptions = {
 	unquoted: false,
 };
 
-export default class AttrQuotes extends Rule<void, RuleOptions> {
+function describeError(context?: RuleContext): string {
+	if (context) {
+		switch (context.error) {
+			case "style":
+				return `Attribute \`${context.attr}\` must use \`${context.expected}\` instead of \`${context.actual}\`.`;
+			case "unquoted":
+				return `Attribute \`${context.attr}\` must not be unquoted.`;
+		}
+	} else {
+		return "This attribute is not quoted properly.";
+	}
+}
+
+function describeStyle(style: QuoteStyle, unquoted: boolean): string {
+	const description: string[] = [];
+	switch (style) {
+		case QuoteStyle.AUTO_QUOTE:
+			description.push(
+				"- quoted with double quotes `\"` unless the value contains double quotes in which case single quotes `'` should be used instead"
+			);
+			break;
+		case QuoteStyle.ANY_QUOTE:
+			description.push("- quoted with single quotes `'`");
+			description.push('- quoted with double quotes `"`');
+			break;
+		case QuoteStyle.SINGLE_QUOTE:
+		case QuoteStyle.DOUBLE_QUOTE: {
+			const name = style === QuoteStyle.SINGLE_QUOTE ? "single" : "double";
+			description.push(`- quoted with ${name} quotes \`${style}\``);
+			break;
+		}
+	}
+	if (unquoted) {
+		description.push("- unquoted (if applicable)");
+	}
+	return `${description.join(" or\n")}\n`;
+}
+
+export default class AttrQuotes extends Rule<RuleContext, RuleOptions> {
 	private style: QuoteStyle;
 
 	public static schema(): SchemaObject {
 		return {
 			style: {
-				enum: ["auto", "double", "single"],
+				enum: ["auto", "double", "single", "any"],
 				type: "string",
 			},
 			unquoted: {
@@ -34,18 +87,20 @@ export default class AttrQuotes extends Rule<void, RuleOptions> {
 		};
 	}
 
-	public documentation(): RuleDocumentation {
-		if (this.options.style === "auto") {
-			return {
-				description: `Attribute values are required to be quoted with doublequotes unless the attribute value itself contains doublequotes in which case singlequotes should be used.`,
-				url: ruleDocumentationUrl(__filename),
-			};
-		} else {
-			return {
-				description: `Attribute values are required to be quoted with ${this.options.style}quotes.`,
-				url: ruleDocumentationUrl(__filename),
-			};
-		}
+	public documentation(context?: RuleContext): RuleDocumentation {
+		const { style } = this;
+		const { unquoted } = this.options;
+		const description = [
+			describeError(context),
+			"",
+			"Under the current configuration attributes must be:",
+			"",
+			describeStyle(style, unquoted),
+		];
+		return {
+			description: description.join("\n"),
+			url: ruleDocumentationUrl(__filename),
+		};
 	}
 
 	public constructor(options: Partial<RuleOptions>) {
@@ -62,23 +117,42 @@ export default class AttrQuotes extends Rule<void, RuleOptions> {
 
 			if (!event.quote) {
 				if (this.options.unquoted === false) {
-					this.report(event.target, `Attribute "${event.key}" using unquoted value`);
+					const message = `Attribute "${event.key}" using unquoted value`;
+					const context: RuleUnquotedContext = {
+						error: "unquoted",
+						attr: event.key,
+					};
+					this.report(event.target, message, null, context);
 				}
+				return;
+			}
+
+			/* if the style is set to any we skip the rest of the rule as the only
+			 * thing that matters is if the "unquoted" options triggers an error or
+			 * not */
+			if (this.style === QuoteStyle.ANY_QUOTE) {
 				return;
 			}
 
 			const expected = this.resolveQuotemark(event.value.toString(), this.style);
 
 			if (event.quote !== expected) {
-				this.report(
-					event.target,
-					`Attribute "${event.key}" used ${event.quote} instead of expected ${expected}`
-				);
+				const message = `Attribute "${event.key}" used ${event.quote} instead of expected ${expected}`;
+				const context: RuleStyleContext = {
+					error: "style",
+					attr: event.key,
+					actual: event.quote,
+					expected,
+				};
+				this.report(event.target, message, null, context);
 			}
 		});
 	}
 
-	private resolveQuotemark(value: string, style: QuoteStyle): QuoteMark {
+	private resolveQuotemark(
+		value: string,
+		style: Exclude<QuoteStyle, QuoteStyle.ANY_QUOTE>
+	): QuoteMark {
 		if (style === QuoteStyle.AUTO_QUOTE) {
 			return value.includes('"') ? "'" : '"';
 		} else {
@@ -95,6 +169,8 @@ function parseStyle(style: string): QuoteStyle {
 			return QuoteStyle.DOUBLE_QUOTE;
 		case "single":
 			return QuoteStyle.SINGLE_QUOTE;
+		case "any":
+			return QuoteStyle.ANY_QUOTE;
 		/* istanbul ignore next: covered by schema validation */
 		default:
 			throw new ConfigError(`Invalid style "${style}" for "attr-quotes" rule`);
