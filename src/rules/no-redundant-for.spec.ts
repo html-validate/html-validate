90b7d003137991da795409376b2498310c2ecff6
David Sveningsson <ext@sidvind.com>
2023-08-20 02:54:42 +0200

    refactor: get test cases up to spec

--- src/rules/no-redundant-for.spec.ts
+++ src/rules/no-redundant-for.spec.ts
@@ -10,54 +10,95 @@ describe("rule no-redundant-for", () => {
 		});
 	});
 
-	it("should not report when <label> does not have for attribute", () => {
+	it("should not report when <label> does not have for attribute", async () => {
 		expect.assertions(1);
-		const report = htmlvalidate.validateString("<label><input></label>");
+		const markup = /* HTML */ `
+			<label>
+				<input />
+			</label>
+		`;
+		const report = await htmlvalidate.validateString(markup);
 		expect(report).toBeValid();
 	});
 
-	it("should not report when <label> references control elsewhere in tree", () => {
+	it("should not report when <label> references control elsewhere in tree", async () => {
 		expect.assertions(1);
-		const report = htmlvalidate.validateString('<label for="foo"></label><input id="foo">');
+		const markup = /* HTML */ `
+			<label for="foo"> </label>
+			<input id="foo" />
+		`;
+		const report = await htmlvalidate.validateString(markup);
 		expect(report).toBeValid();
 	});
 
-	it("should handle omitted value", () => {
+	it("should handle omitted value", async () => {
 		expect.assertions(1);
-		const report = htmlvalidate.validateString("<label for></label>");
+		const markup = /* HTML */ ` <label for> </label> `;
+		const report = await htmlvalidate.validateString(markup);
 		expect(report).toBeValid();
 	});
 
-	it("should handle empty value", () => {
+	it("should handle empty value", async () => {
 		expect.assertions(1);
-		const report = htmlvalidate.validateString('<label for=""></label>');
+		const markup = /* HTML */ ` <label for=""> </label> `;
+		const report = await htmlvalidate.validateString(markup);
 		expect(report).toBeValid();
 	});
 
-	it("should handle whitespace", () => {
+	it("should handle whitespace", async () => {
 		expect.assertions(1);
-		const report = htmlvalidate.validateString('<label for="a b"></label><input id="a b">');
+		const markup = /* HTML */ `
+			<label for="a b"> </label>
+			<input id="a b" />
+		`;
+		const report = await htmlvalidate.validateString(markup);
 		expect(report).toBeValid();
 	});
 
-	it("should not report error for other elements", () => {
+	it("should not report error for other elements", async () => {
 		expect.assertions(1);
-		const report = htmlvalidate.validateString(
-			'<custom-element for="foo"><input id="foo"></custom-element>'
-		);
+		const markup = /* HTML */ `
+			<custom-element for="foo">
+				<input id="foo" />
+			</custom-element>
+		`;
+		const report = await htmlvalidate.validateString(markup);
 		expect(report).toBeValid();
 	});
 
-	it("should report error when <label> references wrapped element", () => {
+	it("should report error when <label> references wrapped element", async () => {
 		expect.assertions(2);
-		const report = htmlvalidate.validateString('<label for="foo"><input id="foo"></label>');
+		const markup = /* HTML */ `
+			<label for="foo">
+				<input id="foo" />
+			</label>
+		`;
+		const report = await htmlvalidate.validateString(markup);
 		expect(report).toBeInvalid();
-		expect(report).toHaveError("no-redundant-for", 'Redundant "for" attribute');
+		expect(report).toMatchInlineCodeframe(`
+			"error: Redundant "for" attribute (no-redundant-for) at inline:2:11:
+			  1 |
+			> 2 | 			<label for="foo">
+			    | 			       ^^^
+			  3 | 				<input id="foo" />
+			  4 | 			</label>
+			  5 |
+			Selector: label"
+		`);
 	});
 
 	it("should contain documentation", async () => {
 		expect.assertions(1);
-		const docs = await htmlvalidate.getRuleDocumentation("no-redundant-for");
-		expect(docs).toMatchSnapshot();
+		const context = undefined;
+		const docs = await htmlvalidate.getContextualDocumentation({
+			ruleId: "no-redundant-for",
+			context,
+		});
+		expect(docs).toMatchInlineSnapshot(`
+			{
+			  "description": "When the \`<label>\` element wraps the labelable control the \`for\` attribute is redundant and better left out.",
+			  "url": "https://html-validate.org/rules/no-redundant-for.html",
+			}
+		`);
 	});
 });
