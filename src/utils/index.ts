e89ca3d3e939a4d497a3eb768874edf34ff9c0df
David Sveningsson <ext@sidvind.com>
2023-12-25 02:29:07 +0100

    refactor(api): move `isThenable` to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export { type CompatibilityOptions } from "./compatibility-check";
 export { computeHash } from "./compute-hash";
 export { interpolate } from "./interpolate";
+export { isThenable } from "./is-thenable";
 export { requireUncached } from "./require-uncached";
 export { ruleExists } from "./rule-exists";
