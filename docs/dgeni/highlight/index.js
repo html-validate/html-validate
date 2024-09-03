cecab7a65c2f2519fde29203b64e14c88bf7379b
David Sveningsson <ext@sidvind.com>
2024-09-03 23:53:37 +0200

    docs: copy unmaintained dgeni-packages/nunjucks implementation

--- docs/dgeni/highlight/index.js
+++ docs/dgeni/highlight/index.js
@@ -1,6 +1,6 @@
 const { Package } = require("dgeni");
 
-module.exports = new Package("highlight", [require("dgeni-packages/nunjucks")])
+module.exports = new Package("highlight", [require("../packages/nunjucks")])
 	.factory(require("./services/highlight"))
 	.factory(require("./filters/highlight"))
 	.config(function (templateEngine, highlightFilter) {
