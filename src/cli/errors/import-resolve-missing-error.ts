8ed0ae8522413164740703137b4f20d70bdeebc5
David Sveningsson <ext@sidvind.com>
2025-01-29 00:34:33 +0100

    fix: better error message when `import.meta.resolve` isnt available

---
+++ src/cli/errors/import-resolve-missing-error.ts
@@ -0,0 +1,31 @@
+import kleur from "kleur";
+import { UserError } from "../../error";
+import { engines } from "../../../package.json";
+
+/**
+ * @internal
+ */
+export class ImportResolveMissingError extends UserError {
+	public constructor() {
+		const message = `import.meta.resolve(..) is not available on this system`;
+		super(message);
+		Error.captureStackTrace(this, ImportResolveMissingError);
+		this.name = ImportResolveMissingError.name;
+	}
+
+	public prettyFormat(): string {
+		const { message } = this;
+		const currentVersion = process.version;
+		const requiredVersion = engines.node
+			.split("||")
+			.map((it) => `v${it.replace(/^[^\d]+/, "").trim()}`);
+		return [
+			kleur.red(`Error: ${message}.`),
+			"",
+			`Either ensure you are running a supported NodeJS version:`,
+			`  Current:  ${currentVersion}`,
+			`  Required: ${requiredVersion.join(", ")}, or later`,
+			`Or set NODE_OPTIONS="--experimental-import-meta-resolve"`,
+		].join("\n");
+	}
+}
--- src/cli/errors/index.ts
+++ src/cli/errors/index.ts
@@ -1,1 +1,2 @@
 export { handleSchemaValidationError } from "./handle-schema-validation-error";
+export { ImportResolveMissingError } from "./import-resolve-missing-error";
