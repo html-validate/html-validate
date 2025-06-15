0e84024fe42f814c3ee776f4370c90715aaafb21
David Sveningsson <ext@sidvind.com>
2025-06-15 22:40:45 +0200

    feat(rules): determine if a table is simple or complex before applying `wcag/h63`
    fixes #303

--- src/rules/wcag/h63.spec.ts
+++ src/rules/wcag/h63.spec.ts
@@ -1,6 +1,440 @@
 import { HtmlValidate } from "../../htmlvalidate";
 import { processAttribute } from "../../transform/mocks/attribute";
 import "../../jest";
+import { Parser } from "../../parser";
+import { Config } from "../../config";
+import { isSimpleTable } from "./h63";
+
+describe("isSimpleTable()", () => {
+	let parser: Parser;
+
+	beforeAll(async () => {
+		const config = Config.empty();
+		parser = new Parser(await config.resolve());
+	});
+
+	describe("headers in first row", () => {
+		it("should return true for table with headings in first row (implicit tbody)", () => {
+			expect.assertions(1);
+			const markup = /* HTML */ `
+				<table>
+					<tr>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+					</tr>
+					<tr>
+						<td>a</td>
+						<td>a</td>
+						<td>a</td>
+					</tr>
+				</table>
+			`;
+			const document = parser.parseHtml(markup);
+			const result = isSimpleTable(document.querySelector("table")!);
+			expect(result).toBe(true);
+		});
+
+		it("should return true for table with headings in first thead row", () => {
+			expect.assertions(1);
+			const markup = /* HTML */ `
+				<table>
+					<thead>
+						<tr>
+							<th>1</th>
+							<th>2</th>
+							<th>3</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>a</td>
+							<td>a</td>
+							<td>a</td>
+						</tr>
+					</tbody>
+				</table>
+			`;
+			const document = parser.parseHtml(markup);
+			const result = isSimpleTable(document.querySelector("table")!);
+			expect(result).toBe(true);
+		});
+
+		it("should return true for table with headings in first tbody row", () => {
+			expect.assertions(1);
+			const markup = /* HTML */ `
+				<table>
+					<tbody>
+						<tr>
+							<th>1</th>
+							<th>2</th>
+							<th>3</th>
+						</tr>
+						<tr>
+							<td>a</td>
+							<td>a</td>
+							<td>a</td>
+						</tr>
+					</tbody>
+				</table>
+			`;
+			const document = parser.parseHtml(markup);
+			const result = isSimpleTable(document.querySelector("table")!);
+			expect(result).toBe(true);
+		});
+
+		it("should return false for table with headings in both thead and tbody", () => {
+			expect.assertions(1);
+			const markup = /* HTML */ `
+				<table>
+					<thead>
+						<tr>
+							<th>1</th>
+							<th>2</th>
+							<th>3</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th>1</th>
+							<th>2</th>
+							<th>3</th>
+						</tr>
+						<tr>
+							<td>a</td>
+							<td>a</td>
+							<td>a</td>
+						</tr>
+					</tbody>
+				</table>
+			`;
+			const document = parser.parseHtml(markup);
+			const result = isSimpleTable(document.querySelector("table")!);
+			expect(result).toBe(false);
+		});
+
+		it("should return false for table with have thead but headings in tbody", () => {
+			expect.assertions(1);
+			const markup = /* HTML */ `
+				<table>
+					<thead>
+						<tr>
+							<td>1</td>
+							<td>2</td>
+							<td>3</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th>1</th>
+							<th>2</th>
+							<th>3</th>
+						</tr>
+						<tr>
+							<td>a</td>
+							<td>a</td>
+							<td>a</td>
+						</tr>
+					</tbody>
+				</table>
+			`;
+			const document = parser.parseHtml(markup);
+			const result = isSimpleTable(document.querySelector("table")!);
+			expect(result).toBe(false);
+		});
+
+		it("should return false for table where not all columns are headers", () => {
+			expect.assertions(1);
+			const markup = /* HTML */ `
+				<table>
+					<thead>
+						<tr>
+							<th>1</th>
+							<th>2</th>
+							<td>3</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>a</td>
+							<td>a</td>
+							<td>a</td>
+						</tr>
+					</tbody>
+				</table>
+			`;
+			const document = parser.parseHtml(markup);
+			const result = isSimpleTable(document.querySelector("table")!);
+			expect(result).toBe(false);
+		});
+	});
+
+	describe("headers in first column", () => {
+		it("should return true for table with headings in first column (implicit tbody)", () => {
+			expect.assertions(1);
+			const markup = /* HTML */ `
+				<table>
+					<tr>
+						<th>1</th>
+						<td>a</td>
+						<td>b</td>
+					</tr>
+					<tr>
+						<th>2</th>
+						<td>c</td>
+						<td>d</td>
+					</tr>
+				</table>
+			`;
+			const document = parser.parseHtml(markup);
+			const result = isSimpleTable(document.querySelector("table")!);
+			expect(result).toBe(true);
+		});
+
+		it("should return true for table with headings in first column with tbody", () => {
+			expect.assertions(1);
+			const markup = /* HTML */ `
+				<table>
+					<tbody>
+						<tr>
+							<th>1</th>
+							<td>a</td>
+							<td>b</td>
+						</tr>
+						<tr>
+							<th>2</th>
+							<td>c</td>
+							<td>d</td>
+						</tr>
+					</tbody>
+				</table>
+			`;
+			const document = parser.parseHtml(markup);
+			const result = isSimpleTable(document.querySelector("table")!);
+			expect(result).toBe(true);
+		});
+
+		it("should return false for table with thead", () => {
+			expect.assertions(1);
+			const markup = /* HTML */ `
+				<table>
+					<thead>
+						<tr>
+							<th>1</th>
+							<td>a</td>
+							<td>b</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th>2</th>
+							<td>c</td>
+							<td>d</td>
+						</tr>
+					</tbody>
+				</table>
+			`;
+			const document = parser.parseHtml(markup);
+			const result = isSimpleTable(document.querySelector("table")!);
+			expect(result).toBe(false);
+		});
+
+		it("should return false for table if not all rows have heading", () => {
+			expect.assertions(1);
+			const markup = /* HTML */ `
+				<table>
+					<tr>
+						<th>1</th>
+						<td>a</td>
+						<td>b</td>
+					</tr>
+					<tr>
+						<th>2</th>
+						<td>c</td>
+						<td>d</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>e</td>
+						<td>f</td>
+					</tr>
+				</table>
+			`;
+			const document = parser.parseHtml(markup);
+			const result = isSimpleTable(document.querySelector("table")!);
+			expect(result).toBe(false);
+		});
+
+		it("should return false for table with headings in other columns", () => {
+			expect.assertions(1);
+			const markup = /* HTML */ `
+				<table>
+					<tbody>
+						<tr>
+							<td>a</td>
+							<th>1</th>
+							<td>b</td>
+						</tr>
+						<tr>
+							<td>c</td>
+							<th>2</th>
+							<td>d</td>
+						</tr>
+						<tr>
+							<td>e</td>
+							<th>3</th>
+							<td>f</td>
+						</tr>
+					</tbody>
+				</table>
+			`;
+			const document = parser.parseHtml(markup);
+			const result = isSimpleTable(document.querySelector("table")!);
+			expect(result).toBe(false);
+		});
+	});
+
+	it("should return false if any cell uses the headings attribute", () => {
+		expect.assertions(1);
+		const markup = /* HTML */ `
+			<table>
+				<tr>
+					<th id="foo">1</th>
+					<th>2</th>
+					<th>3</th>
+				</tr>
+				<tr>
+					<td headers="foo">a</td>
+					<td>a</td>
+					<td>a</td>
+				</tr>
+			</table>
+		`;
+		const document = parser.parseHtml(markup);
+		const result = isSimpleTable(document.querySelector("table")!);
+		expect(result).toBe(false);
+	});
+
+	it("should return false for table without rows", () => {
+		expect.assertions(1);
+		const markup = /* HTML */ ` <table></table> `;
+		const document = parser.parseHtml(markup);
+		const result = isSimpleTable(document.querySelector("table")!);
+		expect(result).toBe(false);
+	});
+
+	it("should return false for thead without rows", () => {
+		expect.assertions(1);
+		const markup = /* HTML */ `
+			<table>
+				<thead></thead>
+			</table>
+		`;
+		const document = parser.parseHtml(markup);
+		const result = isSimpleTable(document.querySelector("table")!);
+		expect(result).toBe(false);
+	});
+
+	it("should return false for tbody without rows", () => {
+		expect.assertions(1);
+		const markup = /* HTML */ `
+			<table>
+				<tbody></tbody>
+			</table>
+		`;
+		const document = parser.parseHtml(markup);
+		const result = isSimpleTable(document.querySelector("table")!);
+		expect(result).toBe(false);
+	});
+
+	it("should return false for tr without cells", () => {
+		expect.assertions(1);
+		const markup = /* HTML */ `
+			<table>
+				<tr></tr>
+			</table>
+		`;
+		const document = parser.parseHtml(markup);
+		const result = isSimpleTable(document.querySelector("table")!);
+		expect(result).toBe(false);
+	});
+
+	it("should handle when column count is mismatched (less)", () => {
+		expect.assertions(1);
+		const markup = /* HTML */ `
+			<table>
+				<tr>
+					<th>1</th>
+					<th>2</th>
+					<th>3</th>
+				</tr>
+				<tr>
+					<td>a</td>
+					<td>b</td>
+				</tr>
+			</table>
+		`;
+		const document = parser.parseHtml(markup);
+		const result = isSimpleTable(document.querySelector("table")!);
+		expect(result).toBe(false);
+	});
+
+	it("should handle when column count is mismatched (more)", () => {
+		expect.assertions(1);
+		const markup = /* HTML */ `
+			<table>
+				<tr>
+					<th>1</th>
+					<th>2</th>
+					<th>3</th>
+				</tr>
+				<tr>
+					<td>a</td>
+					<td>b</td>
+					<td>c</td>
+					<td>d</td>
+				</tr>
+			</table>
+		`;
+		const document = parser.parseHtml(markup);
+		const result = isSimpleTable(document.querySelector("table")!);
+		expect(result).toBe(false);
+	});
+
+	it("should handle nested table", () => {
+		expect.assertions(1);
+		const markup = /* HTML */ `
+			<table>
+				<caption>
+					Simple table
+				</caption>
+				<tr>
+					<th></th>
+				</tr>
+				<tr>
+					<td>
+						<table>
+							<caption>
+								Complex table
+							</caption>
+							<tr>
+								<th></th>
+								<th></th>
+							</tr>
+							<tr>
+								<th></th>
+								<td></td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		`;
+		const document = parser.parseHtml(markup);
+		const result = isSimpleTable(document.querySelector("table")!);
+		expect(result).toBe(true);
+	});
+});
 
 describe("wcag/h63", () => {
 	let htmlvalidate: HtmlValidate;
@@ -12,22 +446,37 @@ describe("wcag/h63", () => {
 		});
 	});
 
-	it("should not report when th has scope attribute", async () => {
+	it("should not report error when simple table has omitted scope attribute", async () => {
 		expect.assertions(1);
 		const markup = /* HTML */ `
+			<!-- all headers in first row -->
 			<table>
 				<tr>
-					<th scope="col"></th>
+					<th>a</th>
+					<th>b</th>
+					<th>c</th>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>2</td>
+					<td>3</td>
+				</tr>
+			</table>
+
+			<!-- all headers in first column -->
+			<table>
+				<tr>
+					<th>a</th>
+					<th>b</th>
+					<th>c</th>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>2</td>
+					<td>3</td>
 				</tr>
 			</table>
 		`;
-		const report = await htmlvalidate.validateString(markup);
-		expect(report).toBeValid();
-	});
-
-	it("should not report for other elements", async () => {
-		expect.assertions(1);
-		const markup = /* HTML */ ` <div></div> `;
 		const report = await htmlvalidate.validateString(markup);
 		expect(report).toBeValid();
 	});
@@ -36,9 +485,19 @@ describe("wcag/h63", () => {
 		expect.assertions(1);
 		const markup = /* HTML */ `
 			<table>
-				<tr>
-					<th dynamic-scope="expr"></th>
-				</tr>
+				<thead>
+					<tr>
+						<!-- force complex table -->
+						<th scope="col"></th>
+						<th scope="col"></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th dynamic-scope="expr"></th>
+						<td></td>
+					</tr>
+				</tbody>
 			</table>
 		`;
 		const report = await htmlvalidate.validateString(markup, { processAttribute });
@@ -49,22 +508,32 @@ describe("wcag/h63", () => {
 		expect.assertions(1);
 		const markup = /* HTML */ `
 			<table>
-				<tr>
-					<th></th>
-				</tr>
+				<thead>
+					<tr>
+						<!-- force complex table -->
+						<th scope="col"></th>
+						<th scope="col"></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th></th>
+						<td></td>
+					</tr>
+				</tbody>
 			</table>
 		`;
 		const report = await htmlvalidate.validateString(markup);
 		expect(report).toMatchInlineCodeframe(`
-			"error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at inline:4:7:
-			  2 | 			<table>
-			  3 | 				<tr>
-			> 4 | 					<th></th>
-			    | 					 ^^
-			  5 | 				</tr>
-			  6 | 			</table>
-			  7 |
-			Selector: table > tr > th"
+			"error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at inline:12:8:
+			  10 | 				<tbody>
+			  11 | 					<tr>
+			> 12 | 						<th></th>
+			     | 						 ^^
+			  13 | 						<td></td>
+			  14 | 					</tr>
+			  15 | 				</tbody>
+			Selector: table > tbody > tr > th"
 		`);
 	});
 
@@ -72,32 +541,41 @@ describe("wcag/h63", () => {
 		expect.assertions(1);
 		const markup = /* HTML */ `
 			<table>
-				<tr>
-					<th scope></th>
-					<th scope=""></th>
-				</tr>
+				<thead>
+					<tr>
+						<!-- force complex table -->
+						<th scope="col"></th>
+						<th scope="col"></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope></th>
+						<th scope=""></th>
+					</tr>
+				</tbody>
 			</table>
 		`;
 		const report = await htmlvalidate.validateString(markup);
 		expect(report).toMatchInlineCodeframe(`
-			"error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at inline:4:10:
-			  2 | 			<table>
-			  3 | 				<tr>
-			> 4 | 					<th scope></th>
-			    | 					    ^^^^^
-			  5 | 					<th scope=""></th>
-			  6 | 				</tr>
-			  7 | 			</table>
-			Selector: table > tr > th:nth-child(1)
-			error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at inline:5:10:
-			  3 | 				<tr>
-			  4 | 					<th scope></th>
-			> 5 | 					<th scope=""></th>
-			    | 					    ^^^^^
-			  6 | 				</tr>
-			  7 | 			</table>
-			  8 |
-			Selector: table > tr > th:nth-child(2)"
+			"error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at inline:12:11:
+			  10 | 				<tbody>
+			  11 | 					<tr>
+			> 12 | 						<th scope></th>
+			     | 						    ^^^^^
+			  13 | 						<th scope=""></th>
+			  14 | 					</tr>
+			  15 | 				</tbody>
+			Selector: table > tbody > tr > th:nth-child(1)
+			error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at inline:13:11:
+			  11 | 					<tr>
+			  12 | 						<th scope></th>
+			> 13 | 						<th scope=""></th>
+			     | 						    ^^^^^
+			  14 | 					</tr>
+			  15 | 				</tbody>
+			  16 | 			</table>
+			Selector: table > tbody > tr > th:nth-child(2)"
 		`);
 	});
 
@@ -105,22 +583,32 @@ describe("wcag/h63", () => {
 		expect.assertions(1);
 		const markup = /* HTML */ `
 			<table>
-				<tr>
-					<th scope="auto"></th>
-				</tr>
+				<thead>
+					<tr>
+						<!-- force complex table -->
+						<th scope="col"></th>
+						<th scope="col"></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="auto"></th>
+						<td></td>
+					</tr>
+				</tbody>
 			</table>
 		`;
 		const report = await htmlvalidate.validateString(markup);
 		expect(report).toMatchInlineCodeframe(`
-			"error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at inline:4:17:
-			  2 | 			<table>
-			  3 | 				<tr>
-			> 4 | 					<th scope="auto"></th>
-			    | 					           ^^^^
-			  5 | 				</tr>
-			  6 | 			</table>
-			  7 |
-			Selector: table > tr > th"
+			"error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at inline:12:18:
+			  10 | 				<tbody>
+			  11 | 					<tr>
+			> 12 | 						<th scope="auto"></th>
+			     | 						           ^^^^
+			  13 | 						<td></td>
+			  14 | 					</tr>
+			  15 | 				</tbody>
+			Selector: table > tbody > tr > th"
 		`);
 	});
 
@@ -128,52 +616,102 @@ describe("wcag/h63", () => {
 		expect.assertions(1);
 		const markup = /* HTML */ `
 			<table>
-				<tr>
-					<th scope="foobar"></th>
-				</tr>
+				<thead>
+					<tr>
+						<!-- force complex table -->
+						<th scope="col"></th>
+						<th scope="col"></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="foobar"></th>
+						<td></td>
+					</tr>
+				</tbody>
 			</table>
 		`;
 		const report = await htmlvalidate.validateString(markup);
 		expect(report).toMatchInlineCodeframe(`
-			"error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at inline:4:17:
-			  2 | 			<table>
-			  3 | 				<tr>
-			> 4 | 					<th scope="foobar"></th>
-			    | 					           ^^^^^^
-			  5 | 				</tr>
-			  6 | 			</table>
-			  7 |
-			Selector: table > tr > th"
+			"error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at inline:12:18:
+			  10 | 				<tbody>
+			  11 | 					<tr>
+			> 12 | 						<th scope="foobar"></th>
+			     | 						           ^^^^^^
+			  13 | 						<td></td>
+			  14 | 					</tr>
+			  15 | 				</tbody>
+			Selector: table > tbody > tr > th"
+		`);
+	});
+
+	it("should report error when table with colspan is missing scope", async () => {
+		expect.assertions(1);
+		const markup = /* HTML */ `
+			<table>
+				<thead>
+					<tr>
+						<!-- force complex table -->
+						<th scope="col"></th>
+						<th scope="col"></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th colspan="2"></th>
+					</tr>
+				</tbody>
+			</table>
+		`;
+		const report = await htmlvalidate.validateString(markup);
+		expect(report).toMatchInlineCodeframe(`
+			"error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at inline:12:8:
+			  10 | 				<tbody>
+			  11 | 					<tr>
+			> 12 | 						<th colspan="2"></th>
+			     | 						 ^^
+			  13 | 					</tr>
+			  14 | 				</tbody>
+			  15 | 			</table>
+			Selector: table > tbody > tr > th"
 		`);
 	});
 
 	it("smoketest", async () => {
 		expect.assertions(1);
+		const htmlvalidate = new HtmlValidate({
+			root: true,
+			rules: { "wcag/h63": "error" },
+		});
 		const report = await htmlvalidate.validateFile("test-files/rules/wcag/h63.html");
 		expect(report).toMatchInlineCodeframe(`
-			"error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at test-files/rules/wcag/h63.html:10:4:
-			   8 |
-			   9 | 		<!-- invalid cases -->
-			> 10 | 		<th></th>
-			     | 		 ^^
-			  11 | 		<th scope="auto"></th>
-			  12 | 	</tr>
-			  13 | </table>
-			Selector: table > tr > th:nth-child(5)
-			error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at test-files/rules/wcag/h63.html:11:14:
-			   9 | 		<!-- invalid cases -->
-			  10 | 		<th></th>
-			> 11 | 		<th scope="auto"></th>
-			     | 		           ^^^^
-			  12 | 	</tr>
-			  13 | </table>
-			  14 |
-			Selector: table > tr > th:nth-child(6)"
+			"error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at test-files/rules/wcag/h63.html:22:5:
+			  20 |
+			  21 | 			<!-- invalid cases -->
+			> 22 | 			<th></th>
+			     | 			 ^^
+			  23 | 			<th scope="auto"></th>
+			  24 | 		</tr>
+			  25 | 	</tbody>
+			Selector: table > tbody > tr > th:nth-child(5)
+			error: <th> element must have a valid scope attribute: row, col, rowgroup or colgroup (wcag/h63) at test-files/rules/wcag/h63.html:23:15:
+			  21 | 			<!-- invalid cases -->
+			  22 | 			<th></th>
+			> 23 | 			<th scope="auto"></th>
+			     | 			           ^^^^
+			  24 | 		</tr>
+			  25 | 	</tbody>
+			  26 | </table>
+			Selector: table > tbody > tr > th:nth-child(6)"
 		`);
 	});
 
 	it("should contain documentation", async () => {
 		expect.assertions(1);
+		const htmlvalidate = new HtmlValidate({
+			root: true,
+			rules: { "wcag/h63": "error" },
+		});
 		const docs = await htmlvalidate.getContextualDocumentation({ ruleId: "wcag/h63" });
 		expect(docs).toMatchSnapshot();
 	});
--- src/rules/wcag/h63.ts
+++ src/rules/wcag/h63.ts
@@ -11,6 +11,69 @@ const { enum: validScopes } = html5.th.attributes?.scope as MetaAttribute & { en
 
 const joinedScopes = naturalJoin(validScopes);
 
+const td = 0;
+const th = 1;
+
+function getShape(cells: number[][]): { rows: number; cols: number } {
+	const rows = cells.length;
+	const cols = cells[0].length;
+	return { rows, cols };
+}
+
+/**
+ * @internal
+ */
+export function isSimpleTable(table: HtmlElement): boolean {
+	/* if any td/th have the headers attribute it is a complex table */
+	const haveHeadersAttr = table.querySelector("> tr > [headers], > tbody > tr > [headers]");
+	if (haveHeadersAttr) {
+		return false;
+	}
+
+	/* if there are no rows in table assume it is a complex table (but wont really
+	 * matter as there will be no cells yet) */
+	const rows = table.querySelectorAll("> tr, > thead > tr, > tbody > tr");
+	if (rows.length === 0) {
+		return false;
+	}
+
+	/* if there are no td/th in the tr assume it is a complex table */
+	const cells = rows.map((tr) => tr.querySelectorAll("> *").map((el) => (el.is("th") ? th : td)));
+	if (cells[0].length === 0) {
+		return false;
+	}
+
+	/* if the number of columns differ in any row assume it is a complex table
+	 * (either it is malformed or it uses col- or rowspan) */
+	const numColumns = cells[0].length;
+	if (!cells.every((row) => row.length === numColumns)) {
+		return false;
+	}
+
+	const shape = getShape(cells);
+	const headersPerRow = cells.map((row) => row.reduce<number>((sum, cell) => sum + cell, 0));
+	const headersPerColumn = Array(shape.cols)
+		.fill(0)
+		.map((_, index) => {
+			return cells.reduce((sum, it) => sum + it[index], 0);
+		});
+
+	/* case 1: all th in first row */
+	const [firstRow, ...otherRows] = headersPerRow;
+	if (firstRow === shape.cols && otherRows.every((row) => row === 0)) {
+		return true;
+	}
+
+	/* case 2: all th in first column */
+	const [firstCol, ...otherCols] = headersPerColumn;
+	const haveThead = Boolean(table.querySelector("> thead"));
+	if (firstCol === shape.rows && otherCols.every((col) => col === 0) && !haveThead) {
+		return true;
+	}
+
+	return false;
+}
+
 export default class H63 extends Rule {
 	public documentation(): RuleDocumentation {
 		return {
@@ -23,7 +86,7 @@ export default class H63 extends Rule {
 	public setup(): void {
 		this.on("element:ready", (event: ElementReadyEvent) => {
 			const node = event.target;
-			if (!node.is("table")) {
+			if (!node.is("table") || isSimpleTable(node)) {
 				return;
 			}
 
