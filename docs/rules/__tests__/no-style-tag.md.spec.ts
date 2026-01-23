0930fd8a6e27ba2db0dd0f94342e3894accd60e6
David Sveningsson <ext@sidvind.com>
2026-01-23 01:55:59 +0100

    feat(rules): add `allowTemplate` option to `no-style`-tag rule
    fixes #329

--- docs/rules/__tests__/no-style-tag.md.spec.ts
+++ docs/rules/__tests__/no-style-tag.md.spec.ts
@@ -7,6 +7,13 @@ markup["incorrect"] = `<style>
     }
 </style>`;
 markup["correct"] = `<link rel="stylesheet" src="my-style.css">`;
+markup["allow-template"] = `<template>
+    <style>
+        :host {
+            display: block;
+        }
+    </style>
+</template>`;
 
 describe("docs/rules/no-style-tag.md", () => {
 	it("inline validation: incorrect", async () => {
@@ -19,6 +26,12 @@ describe("docs/rules/no-style-tag.md", () => {
 		expect.assertions(1);
 		const htmlvalidate = new HtmlValidate({"rules":{"no-style-tag":"error"}});
 		const report = await htmlvalidate.validateString(markup["correct"]);
+		expect(report.results).toMatchSnapshot();
+	});
+	it("inline validation: allow-template", async () => {
+		expect.assertions(1);
+		const htmlvalidate = new HtmlValidate({"rules":{"no-style-tag":["error",{"allowTemplate":true}]}});
+		const report = await htmlvalidate.validateString(markup["allow-template"]);
 		expect(report.results).toMatchSnapshot();
 	});
 });
