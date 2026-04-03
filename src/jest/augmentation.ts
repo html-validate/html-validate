b9fd392e4c514a5af9bbaf378e5fbd65d59e469c
David Sveningsson <ext@sidvind.com>
2026-04-04 00:31:16 +0200

    fix: jest support for explicitly importing `expect` from `@jest/globals`

--- src/jest/augmentation.ts
+++ src/jest/augmentation.ts
@@ -2,6 +2,43 @@
 import { type ConfigData } from "../config";
 import { type Message } from "../message";
 
+declare module "expect" {
+	/* eslint-disable-next-line @typescript-eslint/no-unused-vars -- to match jest declaration */
+	interface Matchers<R, T> {
+		toBeValid(): R;
+		toBeInvalid(): R;
+		toHaveError(error: Partial<Message>): R;
+		toHaveError(ruleId: string, message: string, context?: any): R;
+		toHaveErrors(errors: Array<[string, string] | Record<string, unknown>>): R;
+
+		/**
+		 * Validate string or HTMLElement.
+		 *
+		 * Test passes if result is valid.
+		 *
+		 * @param config - Optional HTML-Validate configuration object.
+		 * @param filename - Optional filename used when matching transformer and
+		 * loading configuration.
+		 */
+		toHTMLValidate(filename?: string): R;
+		toHTMLValidate(config: ConfigData, filename?: string): R;
+		toHTMLValidate(error: Partial<Message>, filename?: string): R;
+		toHTMLValidate(error: Partial<Message>, config: ConfigData, filename?: string): R;
+
+		/**
+		 * Writes out the given [[Report]] using codeframe formatter and compares
+		 * with snapshot.
+		 */
+		toMatchCodeframe(snapshot?: string): R;
+
+		/**
+		 * Writes out the given [[Report]] using codeframe formatter and compares
+		 * with inline snapshot.
+		 */
+		toMatchInlineCodeframe(snapshot?: string): R;
+	}
+}
+
 declare global {
 	/* eslint-disable-next-line @typescript-eslint/no-namespace -- module augmentation */
 	namespace jest {
